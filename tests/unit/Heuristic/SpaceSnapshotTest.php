2de10f92511a173912aae494e07e4781fd716d60
programarivm <info@programarivm.com>
2020-07-05 17:18:01 +0100

    SpaceSnapshotTest is tidied up by using the Benko Gambit

--- tests/unit/Heuristic/SpaceSnapshotTest.php
+++ tests/unit/Heuristic/SpaceSnapshotTest.php
@@ -2,7 +2,9 @@
 
 namespace PGNChess\Tests\Unit\Heuristic;
 
+use PGNChess\Board;
 use PGNChess\Heuristic\SpaceSnapshot;
+use PGNChess\Opening\Benoni\BenkoGambit;
 use PGNChess\PGN\Symbol;
 use PGNChess\Tests\AbstractUnitTestCase;
 
@@ -13,44 +15,50 @@ class SpaceSnapshotTest extends AbstractUnitTestCase
      */
     public function take()
     {
+        $movetext = (new BenkoGambit(new Board))
+                        ->play()
+                        ->getMovetext();
+
+        $snapshot = (new SpaceSnapshot($movetext))->take();
+
         $expected = [
             [
-                Symbol::WHITE => 12,
+                Symbol::WHITE => 14,
                 Symbol::BLACK => 12,
             ],
             [
-                Symbol::WHITE => 16,
-                Symbol::BLACK => 16,
-            ],
-            [
                 Symbol::WHITE => 17,
-                Symbol::BLACK => 16,
-            ],
-            [
-                Symbol::WHITE => 17,
-                Symbol::BLACK => 16,
+                Symbol::BLACK => 15,
             ],
             [
                 Symbol::WHITE => 17,
                 Symbol::BLACK => 17,
             ],
             [
-                Symbol::WHITE => 18,
-                Symbol::BLACK => 18,
+                Symbol::WHITE => 17,
+                Symbol::BLACK => 16,
             ],
             [
-                Symbol::WHITE => 19,
+                Symbol::WHITE => 17,
                 Symbol::BLACK => 20,
             ],
             [
-                Symbol::WHITE => 21,
-                Symbol::BLACK => 18,
+                Symbol::WHITE => 19,
+                Symbol::BLACK => 21,
+            ],
+            [
+                Symbol::WHITE => 22,
+                Symbol::BLACK => 23,
+            ],
+            [
+                Symbol::WHITE => 23,
+                Symbol::BLACK => 20,
+            ],
+            [
+                Symbol::WHITE => 24,
+                Symbol::BLACK => 20,
             ],
         ];
-
-        $snapshot = (new SpaceSnapshot(
-            '1.Nf3 Nf6 2.c4 c5 3.g3 b6 4.Bg2 Bb7 5.O-O e6 6.Nc3 a6 7.d4 cxd4 8.Qxd4 d6'
-        ))->take();
 
         $this->assertEquals($expected, $snapshot);
     }
