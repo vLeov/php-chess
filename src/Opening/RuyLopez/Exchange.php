26cedd00c5c55aea9cc833eef0de81044a0a9001
programarivm <info@programarivm.com>
2020-07-05 12:50:18 +0100

    the exchange variation of the Ruy Lopez is added

---
+++ src/Opening/RuyLopez/Exchange.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PGNChess\Opening\RuyLopez;
+
+use PGNChess\AbstractOpening;
+use PGNChess\PGN\Convert;
+use PGNChess\PGN\Symbol;
+
+class Exchange extends AbstractOpening
+{
+    public function play()
+    {
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'e5'));
+
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Nf3'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Nc6'));
+
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Bb5'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'a6'));
+
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Bxc6'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'dxc6'));
+
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'd4'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'exd4'));
+
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Qxd4'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Qxd4'));
+
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Nxd4'));
+
+        return $this->board;
+    }
+}
