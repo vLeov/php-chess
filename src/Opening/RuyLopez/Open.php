32393276c1bb09eea53557ae0d7833b3f9ca3ac2
programarivm <info@programarivm.com>
2020-07-05 12:24:40 +0100

    open variation of Ruy Lopez is added

---
+++ src/Opening/RuyLopez/Open.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PGNChess\Opening\RuyLopez;
+
+use PGNChess\AbstractOpening;
+use PGNChess\PGN\Convert;
+use PGNChess\PGN\Symbol;
+
+class Open extends AbstractOpening
+{
+    public function play()
+    {
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'e5'));
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Nf3'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Nc6'));
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Bb5'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Nf6'));
+        $this->board->play(Convert::toStdObj(Symbol::WHITE, 'O-O'));
+        $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Nxe4'));
+
+        return $this->board;
+    }
+}
