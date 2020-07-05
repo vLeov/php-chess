b2073b8817a3458920b6ed1d5d9c4bf6eacc8e2d
programarivm <info@programarivm.com>
2020-07-05 16:18:28 +0100

    chess openings cleanup

--- src/Opening/RuyLopez/Exchange.php
+++ src/Opening/RuyLopez/Exchange.php
@@ -12,22 +12,16 @@ class Exchange extends AbstractOpening
     {
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
         $this->board->play(Convert::toStdObj(Symbol::BLACK, 'e5'));
-
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Nf3'));
         $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Nc6'));
-
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Bb5'));
         $this->board->play(Convert::toStdObj(Symbol::BLACK, 'a6'));
-
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Bxc6'));
         $this->board->play(Convert::toStdObj(Symbol::BLACK, 'dxc6'));
-
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'd4'));
         $this->board->play(Convert::toStdObj(Symbol::BLACK, 'exd4'));
-
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Qxd4'));
         $this->board->play(Convert::toStdObj(Symbol::BLACK, 'Qxd4'));
-
         $this->board->play(Convert::toStdObj(Symbol::WHITE, 'Nxd4'));
 
         return $this->board;
