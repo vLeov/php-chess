a17ef91aa2d12f75fd56bdd3ddccb4a8a011d5e4
programarivm <info@programarivm.com>
2020-08-01 13:17:16 +0100

    the supervised regression labeller passes the tests

--- tests/unit/ML/Supervised/Regression/Labeller/Primes/LabellerTest.php
+++ tests/unit/ML/Supervised/Regression/Labeller/Primes/LabellerTest.php
@@ -8,6 +8,10 @@ use PGNChess\ML\Supervised\Regression\Sampler\Primes\Sampler as PrimesSampler;
 use PGNChess\PGN\Convert;
 use PGNChess\PGN\Symbol;
 use PGNChess\Tests\AbstractUnitTestCase;
+use PGNChess\Tests\Sample\Checkmate\Fool as FoolCheckmate;
+use PGNChess\Tests\Sample\Checkmate\Scholar as ScholarCheckmate;
+use PGNChess\Tests\Sample\Opening\Benoni\BenkoGambit;
+use PGNChess\Tests\Sample\Opening\Sicilian\Open as ClosedSicilian;
 use PGNChess\Tests\Sample\Opening\Sicilian\Open as OpenSicilian;
 
 class LabellerTest extends AbstractUnitTestCase
@@ -18,13 +22,111 @@ class LabellerTest extends AbstractUnitTestCase
     public function start()
     {
         $sample = (new PrimesSampler(new Board))->sample();
-        $label = (new PrimesLabeller($sample))->label();
 
         $expected = [
             Symbol::WHITE => 558.66,
             Symbol::BLACK => 558.66,
         ];
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
+    }
+
+    /**
+     * @test
+     */
+    public function w_e4_b_Na6()
+    {
+        $board = new Board;
+        $board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
+        $board->play(Convert::toStdObj(Symbol::BLACK, 'Na6'));
+
+        $sample = (new PrimesSampler($board))->sample();
+
+        $expected = [
+            Symbol::WHITE => 558.66,
+            Symbol::BLACK => 558.66,
+        ];
+
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
+    }
+
+    /**
+     * @test
+     */
+    public function fool_checkmate()
+    {
+        $board = (new FoolCheckmate(new Board))->play();
+        $sample = (new PrimesSampler($board))->sample();
+
+        $expected = [
+            Symbol::WHITE => 565.66,
+            Symbol::BLACK => 548.66,
+        ];
+
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
+    }
+
+    /**
+     * @test
+     */
+    public function scholar_checkmate()
+    {
+        $board = (new ScholarCheckmate(new Board))->play();
+        $sample = (new PrimesSampler($board))->sample();
+
+        $expected = [
+            Symbol::WHITE => 546.66,
+            Symbol::BLACK => 532.66,
+        ];
+
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
+    }
+
+    /**
+     * @test
+     */
+    public function benko_gambit()
+    {
+        $board = (new BenkoGambit(new Board))->play();
+        $sample = (new PrimesSampler($board))->sample();
+
+        $expected = [
+            Symbol::WHITE => 502.03,
+            Symbol::BLACK => 504.03,
+        ];
+
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
+    }
+
+    /**
+     * @test
+     */
+    public function closed_sicilian()
+    {
+        $board = (new ClosedSicilian(new Board))->play();
+        $sample = (new PrimesSampler($board))->sample();
+
+        $expected = [
+            Symbol::WHITE => 549.66,
+            Symbol::BLACK => 556.66,
+        ];
+
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
+    }
+
+    /**
+     * @test
+     */
+    public function open_sicilian()
+    {
+        $board = (new OpenSicilian(new Board))->play();
+        $sample = (new PrimesSampler($board))->sample();
+
+        $expected = [
+            Symbol::WHITE => 549.66,
+            Symbol::BLACK => 556.66,
+        ];
+
+        $this->assertEquals($expected, (new PrimesLabeller($sample))->label());
     }
 }
