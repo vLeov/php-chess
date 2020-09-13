f0bb2590d9c2966e75f4a4506a53a2f1c61b9fe3
programarivm <info@programarivm.com>
2020-09-13 14:29:02 +0100

    two new events are added

---
+++ src/Event/PawnThreateningMajorPiece.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PGNChess\Event;
+
+use PGNChess\PGN\Symbol;
+
+/**
+ * Pawn threatening a major piece.
+ *
+ * @author Jordi Bassagañas
+ * @license GPL
+ */
+class PawnThreateningMajorPiece extends AbstractEvent
+{
+    public function capture(string $color): int
+    {
+        if ($this->board->getHistory()) {
+            $last = array_slice($this->board->getHistory(), -1)[0];
+            if ($last->move->color === $color && $last->move->identity === Symbol::PAWN) {
+                $pawn = $this->board->getPieceByPosition($last->move->position->next);
+                foreach ($pawn->getCaptureSquares() as $square) {
+                    if ($piece = $this->board->getPieceByPosition($square)) {
+                        switch (true) {
+                            case Symbol::oppColor($piece->getColor()) && $piece->getIdentity() === Symbol::QUEEN:
+                                return 1;
+                            case Symbol::oppColor($piece->getColor()) && $piece->getIdentity() === Symbol::ROOK:
+                                return 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
