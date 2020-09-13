d0cbf39658a464d5233f6bd5aad0b983b00a3c7f
programarivm <info@programarivm.com>
2020-09-13 11:46:19 +0100

    a pawn promotion event test has been added

--- tests/unit/Event/PromotionTest.php
+++ tests/unit/Event/PromotionTest.php
@@ -7,6 +7,8 @@ use PGNChess\Castling\Rule as CastlingRule;
 use PGNChess\Event\Promotion as PromotionEvent;
 use PGNChess\Piece\King;
 use PGNChess\Piece\Pawn;
+use PGNChess\Piece\Rook;
+use PGNChess\Piece\Type\RookType;
 use PGNChess\PGN\Convert;
 use PGNChess\PGN\Symbol;
 use PGNChess\Tests\AbstractUnitTestCase;
@@ -51,5 +53,53 @@ class PromotionTest extends AbstractUnitTestCase
 
         $board->play(Convert::toStdObj(Symbol::BLACK, 'b1'));
         $this->assertEquals(1, (new PromotionEvent($board))->capture(Symbol::BLACK));
+    }
+
+    /**
+     * @test
+     */
+    public function bxa8_gxh1()
+    {
+        $pieces = [
+            new Pawn(Symbol::WHITE, 'b6'),
+            new King(Symbol::WHITE, 'd3'),
+            new Rook(Symbol::WHITE, 'h1', RookType::CASTLING_SHORT),
+            new Pawn(Symbol::BLACK, 'g3'),
+            new King(Symbol::BLACK, 'e6'),
+            new Rook(Symbol::BLACK, 'a8', RookType::CASTLING_LONG),
+        ];
+
+        $castling = [
+            Symbol::WHITE => [
+                CastlingRule::IS_CASTLED => true,
+                Symbol::CASTLING_SHORT => false,
+                Symbol::CASTLING_LONG => false
+            ],
+            Symbol::BLACK => [
+                CastlingRule::IS_CASTLED => true,
+                Symbol::CASTLING_SHORT => false,
+                Symbol::CASTLING_LONG => false
+            ]
+        ];
+
+        $board = new Board($pieces, $castling);
+
+        $board->play(Convert::toStdObj(Symbol::WHITE, 'b7'));
+        $this->assertEquals(0, (new PromotionEvent($board))->capture(Symbol::WHITE));
+
+        $board->play(Convert::toStdObj(Symbol::BLACK, 'g2'));
+        $this->assertEquals(0, (new PromotionEvent($board))->capture(Symbol::BLACK));
+
+        $board->play(Convert::toStdObj(Symbol::WHITE, 'bxa8'));
+        $this->assertEquals(1, (new PromotionEvent($board))->capture(Symbol::WHITE));
+
+        $board->play(Convert::toStdObj(Symbol::BLACK, 'gxh1'));
+        $this->assertEquals(1, (new PromotionEvent($board))->capture(Symbol::BLACK));
+
+        $board->play(Convert::toStdObj(Symbol::WHITE, 'Qxh1'));
+        $this->assertEquals(0, (new PromotionEvent($board))->capture(Symbol::WHITE));
+
+        $board->play(Convert::toStdObj(Symbol::BLACK, 'Ke5'));
+        $this->assertEquals(0, (new PromotionEvent($board))->capture(Symbol::BLACK));
     }
 }
