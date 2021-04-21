17df6d71c1ec68352acecdcaed4760bf45dffd4b
programarivm <info@programarivm.com>
2021-04-21 10:58:07 +0200

    Added BasicHeuristicPicture

---
+++ tests/unit/Heuristic/Picture/BasicTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Chess\Tests\Unit\Heuristic\Picture;
+
+use Chess\Board;
+use Chess\Heuristic\Picture\Basic as BasicHeuristicPicture;
+use Chess\PGN\Convert;
+use Chess\PGN\Symbol;
+use Chess\Tests\AbstractUnitTestCase;
+use Chess\Tests\Sample\Opening\Benoni\BenkoGambit;
+use Chess\Tests\Sample\Opening\RuyLopez\Exchange as ExchangeRuyLopez;
+
+class BasicTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function start()
+    {
+        $board = new Board;
+
+        $picture = (new BasicHeuristicPicture($board->getMovetext()))->take();
+
+        $expected = [
+            Symbol::WHITE => [
+                [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+
+            ],
+            Symbol::BLACK => [
+                [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+            ],
+        ];
+
+        $this->assertEquals($expected, $picture);
+    }
+
+    /**
+     * @test
+     */
+    public function w_e4_b_e5()
+    {
+        $board = new Board;
+
+        $board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
+        $board->play(Convert::toStdObj(Symbol::BLACK, 'e5'));
+
+        $picture = (new BasicHeuristicPicture($board->getMovetext()))->take();
+
+        $expected = [
+            Symbol::WHITE => [
+                [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+
+            ],
+            Symbol::BLACK => [
+                [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+            ],
+        ];
+
+        $this->assertEquals($expected, $picture);
+    }
+
+    /**
+     * @test
+     */
+    public function evaluate_benko_gambit()
+    {
+        $board = (new BenkoGambit(new Board))->play();
+
+        $evaluation = (new BasicHeuristicPicture($board->getMovetext()))->evaluate();
+
+        $expected = [
+            Symbol::WHITE => 3.86,
+            Symbol::BLACK => 3.17,
+        ];
+
+        $this->assertEquals($expected, $evaluation);
+    }
+}
