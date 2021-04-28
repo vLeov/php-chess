c88fb42d0e4fd1ce7676d197754e411adc48a793
programarivm <info@programarivm.com>
2021-04-28 11:43:44 +0200

    Computed free squares too in Connectivity

--- src/Evaluation/Connectivity.php
+++ src/Evaluation/Connectivity.php
@@ -16,9 +16,18 @@ class Connectivity extends AbstractEvaluation
 {
     const NAME = 'connectivity';
 
+    private $sqEvald;
+
     public function __construct(Board $board)
     {
         parent::__construct($board);
+
+        $sqEval = new SquareEvaluation($board);
+
+        $this->sqEvald = [
+            SquareEvaluation::FEATURE_FREE => $sqEval->evaluate(SquareEvaluation::FEATURE_FREE),
+            SquareEvaluation::FEATURE_USED => $sqEval->evaluate(SquareEvaluation::FEATURE_USED),
+        ];
 
         $this->result = [
             Symbol::WHITE => 0,
@@ -28,32 +37,39 @@ class Connectivity extends AbstractEvaluation
 
     public function evaluate($feature = null): array
     {
-        $sqEvald = (new SquareEvaluation($this->board))->evaluate(SquareEvaluation::FEATURE_USED);
-
-        $this->color(Symbol::WHITE, $sqEvald);
-        $this->color(Symbol::BLACK, $sqEvald);
+        $this->color(Symbol::WHITE);
+        $this->color(Symbol::BLACK);
 
         return $this->result;
     }
 
-    private function color(string $color, array $sqEvald)
+    private function color(string $color)
     {
         foreach ($this->board->getPiecesByColor($color) as $piece) {
             switch ($piece->getIdentity()) {
                 case Symbol::KING:
-                    $this->result[$color] += count(array_intersect(array_values((array)$piece->getScope()), $sqEvald[$color]));
+                    $this->result[$color] += count(
+                        array_intersect(array_values((array)$piece->getScope()),
+                        $this->sqEvald[SquareEvaluation::FEATURE_USED][$color])
+                    );
                     break;
                 case Symbol::KNIGHT:
-                    $this->result[$color] += count(array_intersect($piece->getScope()->jumps, $sqEvald[$color]));
+                    $this->result[$color] += count(
+                        array_intersect($piece->getScope()->jumps,
+                        $this->sqEvald[SquareEvaluation::FEATURE_USED][$color])
+                    );
                     break;
                 case Symbol::PAWN:
-                    $this->result[$color] += count(array_intersect($piece->getCaptureSquares(), $sqEvald[$color]));
+                    $this->result[$color] += count(
+                        array_intersect($piece->getCaptureSquares(),
+                        $this->sqEvald[SquareEvaluation::FEATURE_USED][$color])
+                    );
                     break;
                 default:
                     $this->result[$color] += count(
                         array_intersect(
                             array_merge(...array_values((array)$piece->getScope())),
-                            $sqEvald[$color]
+                            $this->sqEvald[SquareEvaluation::FEATURE_USED][$color]
                         )
                     );
                     break;
