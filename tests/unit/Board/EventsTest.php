7cfd6ba18d3f2e05292192efd36c33ab3afbfde8
programarivm <info@programarivm.com>
2021-04-30 16:00:01 +0200

    fool_checkmate() passes in EventsTest

--- tests/unit/Board/EventsTest.php
+++ tests/unit/Board/EventsTest.php
@@ -12,10 +12,42 @@ use Chess\Event\PieceCapture as PieceCaptureEvent;
 use Chess\Event\Promotion as PromotionEvent;
 use Chess\PGN\Symbol;
 use Chess\Tests\AbstractUnitTestCase;
+use Chess\Tests\Sample\Checkmate\Fool as FoolCheckmate;
 use Chess\Tests\Sample\Opening\Benoni\BenkoGambit;
 
 class EventsTest extends AbstractUnitTestCase
 {
+    /**
+     * @test
+     */
+    public function fool_checkmate()
+    {
+        $board = (new FoolCheckmate(new Board))->play();
+
+        $expected = (object) [
+            Symbol::WHITE => [
+                CheckEvent::DESC => 0,
+                PieceCaptureEvent::DESC => 0,
+                MajorPieceThreatenedByPawnEvent::DESC => 0,
+                MajorPieceWithinPawnScopeEvent::DESC => 0,
+                MinorPieceThreatenedByPawnEvent::DESC => 0,
+                MinorPieceWithinPawnScopeEvent::DESC => 0,
+                PromotionEvent::DESC => 0,
+            ],
+            Symbol::BLACK => [
+                CheckEvent::DESC => 1,
+                PieceCaptureEvent::DESC => 0,
+                MajorPieceThreatenedByPawnEvent::DESC => 0,
+                MajorPieceWithinPawnScopeEvent::DESC => 0,
+                MinorPieceThreatenedByPawnEvent::DESC => 0,
+                MinorPieceWithinPawnScopeEvent::DESC => 0,
+                PromotionEvent::DESC => 0,
+            ],
+        ];
+
+        $this->assertEquals($expected, $board->events());
+    }
+
     /**
      * @test
      */
