4ebc609564a04403c117da17b3e7b429ed50807b
programarivm <info@programarivm.com>
2021-05-05 17:17:20 +0200

    Added AbstractHeuristicPicture

---
+++ src/Heuristic/Picture/AbstractHeuristicPicture.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Chess\Heuristic\Picture;
+
+use Chess\AbstractPicture;
+use Chess\Evaluation\Value\System;
+use Chess\PGN\Convert;
+use Chess\PGN\Symbol;
+
+class AbstractHeuristicPicture extends AbstractPicture
+{
+    /**
+     * Takes a normalized, heuristic picture.
+     *
+     * @return array
+     */
+    public function take(): array
+    {
+        foreach ($this->moves as $move) {
+            $this->board->play(Convert::toStdObj(Symbol::WHITE, $move[0]));
+            if (isset($move[1])) {
+                $this->board->play(Convert::toStdObj(Symbol::BLACK, $move[1]));
+            }
+            $item = [];
+            foreach ($this->dimensions as $dimension) {
+                $evald = (new $dimension($this->board))->evaluate(System::SYSTEM_BERLINER);
+                is_array($evald[Symbol::WHITE])
+                    ? $item[] = [
+                        Symbol::WHITE => count($evald[Symbol::WHITE]),
+                        Symbol::BLACK => count($evald[Symbol::BLACK])]
+                    : $item[] = $evald;
+            }
+            $this->picture[Symbol::WHITE][] = array_column($item, Symbol::WHITE);
+            $this->picture[Symbol::BLACK][] = array_column($item, Symbol::BLACK);
+        }
+
+        $this->normalize();
+
+        return $this->picture;
+    }
+}
--- src/Heuristic/Picture/Standard.php
+++ src/Heuristic/Picture/Standard.php
@@ -2,8 +2,6 @@
 
 namespace Chess\Heuristic\Picture;
 
-use Chess\AbstractPicture;
-use Chess\Evaluation\Value\System;
 use Chess\Evaluation\Attack as AttackEvaluation;
 use Chess\Evaluation\Center as CenterEvaluation;
 use Chess\Evaluation\Connectivity as ConnectivityEvaluation;
@@ -11,10 +9,8 @@ use Chess\Evaluation\KingSafety as KingSafetyEvaluation;
 use Chess\Evaluation\Material as MaterialEvaluation;
 use Chess\Evaluation\Pressure as PressureEvaluation;
 use Chess\Evaluation\Space as SpaceEvaluation;
-use Chess\PGN\Convert;
-use Chess\PGN\Symbol;
 
-class Standard extends AbstractPicture
+class Standard extends AbstractHeuristicPicture
 {
     protected $dimensions = [
         MaterialEvaluation::class,
@@ -25,34 +21,4 @@ class Standard extends AbstractPicture
         PressureEvaluation::class,
         AttackEvaluation::class,
     ];
-
-    /**
-     * Takes a normalized, heuristic picture.
-     *
-     * @return array
-     */
-    public function take(): array
-    {
-        foreach ($this->moves as $move) {
-            $this->board->play(Convert::toStdObj(Symbol::WHITE, $move[0]));
-            if (isset($move[1])) {
-                $this->board->play(Convert::toStdObj(Symbol::BLACK, $move[1]));
-            }
-            $item = [];
-            foreach ($this->dimensions as $dimension) {
-                $evald = (new $dimension($this->board))->evaluate(System::SYSTEM_BERLINER);
-                is_array($evald[Symbol::WHITE])
-                    ? $item[] = [
-                        Symbol::WHITE => count($evald[Symbol::WHITE]),
-                        Symbol::BLACK => count($evald[Symbol::BLACK])]
-                    : $item[] = $evald;
-            }
-            $this->picture[Symbol::WHITE][] = array_column($item, Symbol::WHITE);
-            $this->picture[Symbol::BLACK][] = array_column($item, Symbol::BLACK);
-        }
-
-        $this->normalize();
-
-        return $this->picture;
-    }
 }
