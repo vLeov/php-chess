9362e68682eb79b75bfb5a69018a40765c20886b
programarivm <info@programarivm.com>
2021-05-06 10:23:02 +0200

    Updated the heuristic pictures with new evaluations

--- src/Heuristic/Picture/Attacking.php
+++ src/Heuristic/Picture/Attacking.php
@@ -5,6 +5,8 @@ namespace Chess\Heuristic\Picture;
 use Chess\Evaluation\Attack as AttackEvaluation;
 use Chess\Evaluation\Center as CenterEvaluation;
 use Chess\Evaluation\Connectivity as ConnectivityEvaluation;
+use Chess\Evaluation\Ease as EaseEvaluation;
+use Chess\Evaluation\Guard as GuardEvaluation;
 use Chess\Evaluation\KingSafety as KingSafetyEvaluation;
 use Chess\Evaluation\Material as MaterialEvaluation;
 use Chess\Evaluation\Pressure as PressureEvaluation;
@@ -16,6 +18,8 @@ class Attacking extends AbstractHeuristicPicture
         MaterialEvaluation::class,
         AttackEvaluation::class,
         PressureEvaluation::class,
+        GuardEvaluation::class,
+        EaseEvaluation::class,
         KingSafetyEvaluation::class,
         ConnectivityEvaluation::class,
         SpaceEvaluation::class,
--- src/Heuristic/Picture/Defensive.php
+++ src/Heuristic/Picture/Defensive.php
@@ -5,6 +5,8 @@ namespace Chess\Heuristic\Picture;
 use Chess\Evaluation\Attack as AttackEvaluation;
 use Chess\Evaluation\Center as CenterEvaluation;
 use Chess\Evaluation\Connectivity as ConnectivityEvaluation;
+use Chess\Evaluation\Ease as EaseEvaluation;
+use Chess\Evaluation\Guard as GuardEvaluation;
 use Chess\Evaluation\KingSafety as KingSafetyEvaluation;
 use Chess\Evaluation\Material as MaterialEvaluation;
 use Chess\Evaluation\Pressure as PressureEvaluation;
@@ -15,6 +17,8 @@ class Defensive extends AbstractHeuristicPicture
     protected $dimensions = [
         KingSafetyEvaluation::class,
         MaterialEvaluation::class,
+        GuardEvaluation::class,
+        EaseEvaluation::class,
         ConnectivityEvaluation::class,
         SpaceEvaluation::class,
         CenterEvaluation::class,
