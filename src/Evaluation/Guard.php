d475c79ff1d5f7430d0aaa48f4585616c8db8411
programarivm <info@programarivm.com>
2021-05-06 09:39:43 +0200

    Implemented Chess\Evaluation\Guard

---
+++ src/Evaluation/Guard.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Chess\Evaluation;
+
+use Chess\Board;
+use Chess\Evaluation\Attack as AttackEvaluation;
+use Chess\PGN\Symbol;
+
+/**
+ * Guard evaluation.
+ *
+ * @author Jordi Bassagañas
+ * @license GPL
+ */
+class Guard extends AbstractEvaluation
+{
+    const NAME = 'guard';
+
+    private $attackEvald;
+
+    public function __construct(Board $board)
+    {
+        parent::__construct($board);
+
+        $this->attackEvald = (new AttackEvaluation($board))->evaluate();
+
+        $this->result = [
+            Symbol::WHITE => 32,
+            Symbol::BLACK => 32,
+        ];
+    }
+
+    public function evaluate($feature = null): array
+    {
+        $this->result[Symbol::WHITE] -= $this->attackEvald[Symbol::BLACK];
+        $this->result[Symbol::BLACK] -= $this->attackEvald[Symbol::WHITE];
+
+        return $this->result;
+    }
+}
---
+++ tests/unit/Evaluation/GuardTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Chess\Tests\Unit\Evaluation;
+
+use Chess\Board;
+use Chess\PGN\Symbol;
+use Chess\Evaluation\Guard as GuardEvaluation;
+use Chess\Tests\AbstractUnitTestCase;
+use Chess\Tests\Sample\Opening\Sicilian\Closed as ClosedSicilian;
+use Chess\Tests\Sample\Opening\Sicilian\Open as OpenSicilian;
+
+class GuardTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function start()
+    {
+        $guardEvald = (new GuardEvaluation(new Board()))->evaluate();
+
+        $expected = [
+            Symbol::WHITE => 32,
+            Symbol::BLACK => 32,
+        ];
+
+        $this->assertEquals($expected, $guardEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function open_sicilian()
+    {
+        $board = (new OpenSicilian(new Board()))->play();
+
+        $guardEvald = (new GuardEvaluation($board))->evaluate();
+
+        $expected = [
+            Symbol::WHITE => 31,
+            Symbol::BLACK => 32,
+        ];
+
+        $this->assertEquals($expected, $guardEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function closed_sicilian()
+    {
+        $board = (new ClosedSicilian(new Board()))->play();
+
+        $guardEvald = (new GuardEvaluation($board))->evaluate();
+
+        $expected = [
+            Symbol::WHITE => 28.8,
+            Symbol::BLACK => 32,
+        ];
+
+        $this->assertEquals($expected, $guardEvald);
+    }
+}
