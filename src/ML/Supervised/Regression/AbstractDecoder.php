296b1fcc025ee28a5e12b2657863375bb6dbfad8
programarivm <info@programarivm.com>
2021-05-14 12:03:34 +0200

    Cleaned up AbstractDecoder

--- src/ML/Supervised/Regression/AbstractDecoder.php
+++ src/ML/Supervised/Regression/AbstractDecoder.php
@@ -3,6 +3,8 @@
 namespace Chess\ML\Supervised\Regression;
 
 use Chess\Board;
+use Chess\PGN\Convert;
+use Chess\PGN\Symbol;
 
 /**
  * AbstractDecoder
@@ -10,19 +12,57 @@ use Chess\Board;
  * @author Jordi Bassagañas
  * @license GPL
  */
-class AbstractDecoder
+abstract class AbstractDecoder
 {
     protected $board;
-
-    protected $labeller;
-
-    protected $heuristicPicture;
 
     protected $result = [];
 
     public function __construct(Board $board)
     {
         $this->board = $board;
+    }
+
+    public function decode(string $color, float $float): string
+    {
+        foreach ($this->board->getPiecesByColor($color) as $piece) {
+            foreach ($piece->getLegalMoves() as $square) {
+                $clone = unserialize(serialize($this->board));
+                switch ($piece->getIdentity()) {
+                    case Symbol::KING:
+                        if ($clone->play(Convert::toStdObj($color, Symbol::CASTLING_SHORT))) {
+                            $this->result[] = [ Symbol::CASTLING_SHORT => $this->label($clone, $color) ];
+                        } elseif ($clone->play(Convert::toStdObj($color, Symbol::CASTLING_LONG))) {
+                            $this->result[] = [ Symbol::CASTLING_LONG => $this->label($clone, $color) ];
+                        } elseif ($clone->play(Convert::toStdObj($color, Symbol::KING.$square))) {
+                            $this->result[] = [ Symbol::KING.$square => $this->label($clone, $color) ];
+                        } elseif ($clone->play(Convert::toStdObj($color, Symbol::KING.'x'.$square))) {
+                            $this->result[] = [ Symbol::KING.'x'.$square => $this->label($clone, $color) ];
+                        }
+                        break;
+                    case Symbol::PAWN:
+                        if ($clone->play(Convert::toStdObj($color, $square))) {
+                            $this->result[] = [ $square => $this->label($clone, $color) ];
+                        } elseif ($clone->play(Convert::toStdObj($color, $piece->getFile()."x$square"))) {
+                            $this->result[] = [ $piece->getFile()."x$square" => $this->label($clone, $color) ];
+                        }
+                        break;
+                    default:
+                        if ($clone->play(Convert::toStdObj($color, $piece->getIdentity().$square))) {
+                            $this->result[] = [ $piece->getIdentity().$square => $this->label($clone, $color) ];
+                        } elseif ($clone->play(Convert::toStdObj($color, "{$piece->getIdentity()}x$square"))) {
+                            $this->result[] = [ "{$piece->getIdentity()}x$square" => $this->label($clone, $color) ];
+                        }
+                        break;
+                }
+            }
+        }
+
+        usort($this->result, function ($a, $b) {
+            return current($b) <=> current($a);
+        });
+
+        return $this->pgn($this->closest($float));
     }
 
     protected function closest(float $search)
@@ -45,13 +85,5 @@ class AbstractDecoder
         }
     }
 
-    protected function label(Board $clone, string $color)
-    {
-        $heuristicPicture = new $this->heuristicPicture($clone->getMovetext());
-        $sample = $heuristicPicture->sample();
-        $weights = array_values($heuristicPicture->getDimensions());
-        $label = (new $this->labeller($sample, $weights))->label()[$color];
-
-        return $label;
-    }
+    abstract protected function label(Board $clone, string $color);
 }
