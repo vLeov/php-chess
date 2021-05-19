7a678e76fb9e5852086fb54cc83597aab75c03a8
programarivm <info@programarivm.com>
2021-05-19 09:58:19 +0200

    Tested balance in scholar_checkmate()

--- tests/unit/Heuristic/Picture/BalanceTest.php
+++ tests/unit/Heuristic/Picture/BalanceTest.php
@@ -63,4 +63,23 @@ class BalanceTest extends AbstractUnitTestCase
 
         $this->assertEquals($expected, $balance);
     }
+
+    /**
+     * @test
+     */
+    public function scholar_checkmate()
+    {
+        $board = (new ScholarCheckmate(new Board()))->play();
+
+        $balance = (new HeuristicPicture($board->getMovetext()))->balance();
+
+        $expected = [
+            [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+            [ 0, 0, -0.35, 0, 0.2, 0.25, 0, 0 ],
+            [ 0, -1, -1, 0.8, 0.4, 0.25, 0, -0.8 ],
+            [ 1, -1, -0.86, 0.4, 0.6, 1, -0.5, -0.1 ],
+        ];
+
+        $this->assertEquals($expected, $balance);
+    }
 }
