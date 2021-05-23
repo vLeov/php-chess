c4145be240ee7d9ac34fc5c2b9a5264fbec72d53
programarivm <info@programarivm.com>
2021-05-23 21:29:30 +0200

    Restructured tests

--- tests/unit/ML/Supervised/OptimalLinearCombinationLabellerTest.php
+++ tests/unit/ML/Supervised/OptimalLinearCombinationLabellerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Chess\Tests\Unit\ML\Supervised\Regression;
+namespace Chess\Tests\Unit\ML\Supervised;
 
 use Chess\Board;
 use Chess\Combinatorics\RestrictedPermutationWithRepetition;
@@ -33,177 +33,191 @@ class OptimalLinearCombinationLabellerTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function start_labelled()
+    public function start_balanced()
     {
         $board = new Board();
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 50,
-            Symbol::BLACK => 50,
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 0,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))
-            ->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function w_e4_b_e5_labelled()
+    public function w_e4_b_e5_balanced()
     {
         $board = new Board();
         $board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
         $board->play(Convert::toStdObj(Symbol::BLACK, 'e5'));
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 50,
-            Symbol::BLACK => 50,
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 0,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))
-            ->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function w_e4_b_Na6_labelled()
+    public function w_e4_b_Na6_balanced()
     {
         $board = new Board();
         $board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
         $board->play(Convert::toStdObj(Symbol::BLACK, 'Na6'));
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 72,
-            Symbol::BLACK => 61.5,
+            Symbol::WHITE => 44,
+            Symbol::BLACK => -23,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))
-            ->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function w_e4_b_Nc6_labelled()
+    public function w_e4_b_Nc6_balanced()
     {
         $board = new Board();
         $board->play(Convert::toStdObj(Symbol::WHITE, 'e4'));
         $board->play(Convert::toStdObj(Symbol::BLACK, 'Nc6'));
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 59,
-            Symbol::BLACK => 69.5,
+            Symbol::WHITE => 18,
+            Symbol::BLACK => -39,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function fool_checkmate_labelled()
+    public function fool_checkmate_balanced()
     {
         $board = (new FoolCheckmate(new Board()))->play();
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 38.7,
-            Symbol::BLACK => 80,
+            Symbol::WHITE => 0,
+            Symbol::BLACK => -67.2,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function scholar_checkmate_labelled()
+    public function scholar_checkmate_balanced()
     {
         $board = (new ScholarCheckmate(new Board()))->play();
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 66.96,
-            Symbol::BLACK => 59.79,
+            Symbol::WHITE => 38.32,
+            Symbol::BLACK => -28.48,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function benko_gambit_labelled()
+    public function benko_gambit_balanced()
     {
         $board = (new BenkoGambit(new Board()))->play();
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 63.52,
-            Symbol::BLACK => 55.5,
+            Symbol::WHITE => 33.72,
+            Symbol::BLACK => -15.03,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
     }
 
     /**
      * @test
      */
-    public function closed_sicilian_labelled()
+    public function closed_sicilian_balanced()
     {
         $board = (new ClosedSicilian(new Board()))->play();
 
         $end = (new HeuristicPicture($board->getMovetext()))
-            ->take()
+            ->takeBalanced()
             ->end();
 
         $expected = [
-            Symbol::WHITE => 55.21,
-            Symbol::BLACK => 43.92,
+            Symbol::WHITE => 27.06,
+            Symbol::BLACK => -10.05,
         ];
 
-        $label = (new OptimalLinearCombinationLabeller(self::$permutations))->label($end);
+        $balance = (new OptimalLinearCombinationLabeller(self::$permutations))->balance($end);
 
-        $this->assertEquals($expected, $label);
+        $this->assertEquals($expected, $balance);
+    }
+
+    /**
+     * @test
+     */
+    public function closed_sicilian_permuted()
+    {
+        $board = (new ClosedSicilian(new Board()))->play();
+
+        $end = (new HeuristicPicture($board->getMovetext()))->take()->end();
+
+        $expected = [ 8, 8, 34, 13, 13, 8, 8, 8 ];
+
+        $permutation = (new OptimalLinearCombinationLabeller(self::$permutations))
+            ->permute($end, Symbol::BLACK, 43.92);
+
+        $this->assertEquals($expected, $permutation);
     }
 }
