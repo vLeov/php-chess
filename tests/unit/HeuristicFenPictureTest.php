c007179a78b20ecc86ea081a89e198ed2fd93fba
programarivm <info@programarivm.com>
2021-05-30 23:23:30 +0200

    Added FEN tests

--- tests/unit/HeuristicFenPictureTest.php
+++ tests/unit/HeuristicFenPictureTest.php
@@ -50,6 +50,79 @@ class HeuristicFenPictureTest extends AbstractUnitTestCase
     /**
      * @test
      */
+    public function e4_e5_evaluate()
+    {
+        $fen = (new Fen('rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2'))
+            ->load();
+
+        $evaluation = (new HeuristicFenPicture($fen))->evaluate();
+
+        $expected = [
+            Symbol::WHITE => 43.21,
+            Symbol::BLACK => 43.21,
+        ];
+
+        $this->assertEquals($expected, $evaluation);
+    }
+
+    /**
+     * @test
+     */
+    public function e4_e5_Nf3_Nf6_take_get_picture()
+    {
+        $fen = (new Fen('rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3'))
+            ->load();
+
+        $pic = (new HeuristicFenPicture($fen))
+            ->take()
+            ->getPicture();
+
+        $expected = [
+            Symbol::WHITE => [ 1, 0.05, 0.51, 0.41, 0, 0, 0, 0 ],
+            Symbol::BLACK => [ 1, 0.05, 0.51, 0.41, 0, 0, 0, 0 ],
+        ];
+
+        $this->assertEquals($expected, $pic);
+    }
+
+    /**
+     * @test
+     */
+    public function e4_e5_Nf3_Nf6_take_get_balance()
+    {
+        $fen = (new Fen('rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3'))
+            ->load();
+
+        $balance = (new HeuristicFenPicture($fen))
+            ->take()
+            ->getBalance();
+
+        $expected = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
+
+        $this->assertEquals($expected, $balance);
+    }
+
+    /**
+     * @test
+     */
+    public function e4_e5_Nf3_Nf6_evaluate()
+    {
+        $fen = (new Fen('rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3'))
+            ->load();
+
+        $evaluation = (new HeuristicFenPicture($fen))->evaluate();
+
+        $expected = [
+            Symbol::WHITE => 44.56,
+            Symbol::BLACK => 44.56,
+        ];
+
+        $this->assertEquals($expected, $evaluation);
+    }
+
+    /**
+     * @test
+     */
     public function benko_gambit_take_get_picture()
     {
         $fen = (new Fen('rn1qkb1r/4pp1p/3p1np1/2pP4/4P3/2N3P1/PP3P1P/R1BQ1KNR b kq - 0 9'))
