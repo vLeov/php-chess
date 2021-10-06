bde6bbb97c54d1092e36d0f7020aa862c4f66b80
bofalke <54977705+bofalke@users.noreply.github.com>
2021-10-06 21:12:31 +0200

    Add backward pawn evaluation (#80)
    * Add backward pawn evaluation
    
    * Update BackwardPawnEvaluationTest.php
    
    Co-authored-by: Jordi Bassaganas <info@programarivm.com>

---
+++ src/Evaluation/BackwardPawnEvaluation.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Chess\Evaluation;
+
+use Chess\Board;
+use Chess\Evaluation\SpaceEvaluation;
+use Chess\PGN\Symbol;
+
+/**
+ * Backward Pawn
+ *
+ * @author Boas Falke
+ * @license GPL
+ */
+class BackwardPawnEvaluation extends AbstractEvaluation
+{
+    const NAME = 'backward_pawn';
+
+    public function __construct(Board $board)
+    {
+        parent::__construct($board);
+
+        $this->result = [
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 0,
+        ];
+    }
+
+    public function evaluate($feature = null): array
+    {
+        $captureSquares = [];
+        $nextMoves = [];
+        foreach ($this->board->getPieces() as $piece) {
+            if ($piece->getIdentity() === Symbol::PAWN) {
+                $captureSquares[] = [
+                    'color' => $piece->getColor(),
+                    'captureSquares' => $piece->getCaptureSquares(),
+                ];
+
+                //Only check for movable pawns and their next possible square
+                if (0 === count($piece->getLegalMoves()) || !str_contains($piece->getLegalMoves()[0], $piece->getFile())) {
+                    continue;
+                }
+                $nextMoves[] = [
+                    'color' => $piece->getColor(),
+                    'nextSquare' => $piece->getLegalMoves()[0],
+                    'nextSquareDefendedByPawn' => false,
+                ];
+            }
+        }
+
+        if (0 === count($nextMoves) || 0 === count($captureSquares)) {
+            return $this->result;
+        }
+
+        $nextMoves = array_map(static function ($nextMove) use ($captureSquares) {
+            foreach ($captureSquares as $captureSquare) {
+                if (
+                    $nextMove['color'] === $captureSquare['color'] &&
+                    in_array($nextMove['nextSquare'], $captureSquare['captureSquares'], true)
+                )
+                {
+                    $nextMove['nextSquareDefendedByPawn'] = true;
+                    return $nextMove;
+                }
+            }
+            return $nextMove;
+        }, $nextMoves);
+
+        foreach ($nextMoves as $nextMove) {
+            foreach ($captureSquares as $captureSquare) {
+                if ($nextMove['nextSquareDefendedByPawn']) {
+                    continue;
+                }
+                if (
+                    $nextMove['color'] !== $captureSquare['color'] &&
+                        in_array($nextMove['nextSquare'], $captureSquare['captureSquares'], true)
+                ) {
+                    ++$this->result[$nextMove['color']];
+                }
+            }
+        }
+        return $this->result;
+    }
+}
---
+++ tests/unit/Evaluation/BackwardPawnEvaluationTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Chess\Tests\Unit\Evaluation;
+
+use Chess\Ascii;
+use Chess\Evaluation\BackwardPawnEvaluation;
+use Chess\Evaluation\DoubledPawnEvaluation;
+use Chess\PGN\Symbol;
+use Chess\Tests\AbstractUnitTestCase;
+
+class BackwardPawnEvaluationTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function kaufman_16()
+    {
+        $position = [
+            7 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            6 => [ ' . ', ' . ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ' ],
+            5 => [ ' p ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' p ', ' P ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' P ', ' . ', ' P ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' P ', ' . ', ' . ', ' . ', ' . ', ' k ', ' . ' ],
+            1 => [ ' . ', ' P ', ' . ', ' K ', ' . ', ' . ', ' . ', ' . ' ],
+            0 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+        ];
+
+        $board = (new Ascii())->toBoard($position, Symbol::WHITE);
+
+        $expected = [
+            Symbol::WHITE => 2,
+            Symbol::BLACK => 1,
+        ];
+
+        $backwardPawnEvald = (new BackwardPawnEvaluation($board))->evaluate();
+
+        $this->assertEquals($expected, $backwardPawnEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function kaufman_16_recognizes_defended_pawns(): void
+    {
+        $position = [
+            7 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            6 => [ ' . ', ' . ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ' ],
+            5 => [ ' p ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' p ', ' P ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' P ', ' P ', ' P ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' k ', ' . ' ],
+            1 => [ ' . ', ' P ', ' . ', ' K ', ' . ', ' . ', ' . ', ' . ' ],
+            0 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+        ];
+
+        $board = (new Ascii())->toBoard($position, Symbol::WHITE);
+
+        $expected = [
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 1,
+        ];
+
+        $backwardPawnEvald = (new BackwardPawnEvaluation($board))->evaluate();
+
+        $this->assertEquals($expected, $backwardPawnEvald);
+    }
+}
