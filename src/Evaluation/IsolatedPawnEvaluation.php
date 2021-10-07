201214ad280b120be2fd718c0fcbdfa95fb63cd5
Viktor <vikengui@gmail.com>
2021-10-07 17:56:50 +0800

    Implement the isolated pawn evaluation #52 (#85)
    * Implement the isolated pawn evaluation #52
    
    * Update IsolatedPawnEvaluationTest.php
    
    Co-authored-by: Jordi Bassaganas <info@programarivm.com>

---
+++ src/Evaluation/IsolatedPawnEvaluation.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Chess\Evaluation;
+
+use Chess\Board;
+use Chess\PGN\Symbol;
+use Chess\Piece\Pawn;
+
+class IsolatedPawnEvaluation extends AbstractEvaluation
+{
+    const NAME = 'isolated_pawn';
+
+    public function __construct(Board $board)
+    {
+        parent::__construct($board);
+
+        $this->result = [
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 0,
+        ];
+    }
+
+    public function evaluate($feature = null): array
+    {
+        foreach ($this->board->getPieces() as $piece) {
+            $color = $piece->getColor();
+            /** @var Pawn $piece */
+            if ($piece->getIdentity() === Symbol::PAWN) {
+                $this->result[ $color ] += $this->checkIsolatedPawn($piece);
+            }
+        }
+        return $this->result;
+    }
+
+    private function checkIsolatedPawn(Pawn $pawn): int
+    {
+        $color = $pawn->getColor();
+        $pawnFile = $pawn->getFile();
+
+        $prevFile = chr(ord($pawnFile) - 1);
+        $nextFile = chr(ord($pawnFile) + 1);
+
+        $squares = [];
+        foreach ([ $prevFile, $nextFile ] as $file) {
+            if ($file < 'a' || $file > 'h') {
+                continue;
+            }
+            $ranks = range(2, 7);
+            $squaresFile = array_map(function($rank) use ($file){
+                return $file . $rank;
+            }, $ranks);
+            $squares = array_merge($squares, $squaresFile);
+        }
+
+        foreach ($squares as $square) {
+            if ($nextPiece = $this->board->getPieceByPosition($square)) {
+                if ($nextPiece->getIdentity() === Symbol::PAWN && $nextPiece->getColor() === $color) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}
---
+++ tests/unit/Evaluation/IsolatedPawnEvaluationTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Chess\Tests\Unit\Evaluation;
+
+use Chess\Ascii;
+use Chess\PGN\Symbol;
+use Chess\Tests\AbstractUnitTestCase;
+use Chess\Evaluation\IsolatedPawnEvaluation;
+
+class IsolatedPawnEvaluationTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function kaufman_09()
+    {
+        $position = [
+            7 => [ ' r ', ' . ', ' . ', ' . ', ' k ', ' . ', ' . ', ' r ' ],
+            6 => [ ' p ', ' b ', ' n ', ' . ', ' . ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' P ', ' . ', ' p ', ' P ', ' . ', ' . ', ' . ' ],
+            3 => [ ' P ', ' . ', ' q ', ' P ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' B ', ' . ', ' . ' ],
+            1 => [ ' . ', ' . ', ' . ', ' Q ', ' . ', ' P ', ' P ', ' P ' ],
+            0 => [ ' R ', ' . ', ' . ', ' . ', ' K ', ' . ', ' . ', ' R ' ],
+        ];
+
+        $board = (new Ascii())->toBoard($position, Symbol::WHITE);
+
+        $expected = [
+            Symbol::WHITE => 0,
+            Symbol::BLACK => 2,
+        ];
+
+        $isolatedPawnEvald = (new IsolatedPawnEvaluation($board))->evaluate();
+
+        $this->assertEquals($expected, $isolatedPawnEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function kaufman_13()
+    {
+        $position = [
+            7 => [ ' . ', ' r ', ' . ', ' . ', ' . ', ' . ', ' k ', ' . ' ],
+            6 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' n ', ' p ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ', ' n ' ],
+            3 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' N ', ' B ', ' . ', ' . ', ' . ', ' . ' ],
+            1 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' P ' ],
+            0 => [ ' . ', ' . ', ' . ', ' N ', ' . ', ' R ', ' K ', ' . ' ],
+        ];
+
+        $board = (new Ascii())->toBoard($position, Symbol::WHITE);
+
+        $expected = [
+            Symbol::WHITE => 1,
+            Symbol::BLACK => 1,
+        ];
+
+        $isolatedPawnEvald = (new IsolatedPawnEvaluation($board))->evaluate();
+
+        $this->assertEquals($expected, $isolatedPawnEvald);
+    }
+
+    /**
+     * @test
+     */
+    public function kaufman_14()
+    {
+        $position = [
+            7 => [ ' . ', ' r ', ' . ', ' . ', ' r ', ' . ', ' k ', ' . ' ],
+            6 => [ ' p ', ' . ', ' . ', ' . ', ' . ', ' p ', ' . ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' p ', ' B ' ],
+            4 => [ ' q ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' . ', ' Q ', ' . ', ' P ', ' . ' ],
+            1 => [ ' P ', ' b ', ' P ', ' . ', ' . ', ' P ', ' K ', ' P ' ],
+            0 => [ ' . ', ' R ', ' . ', ' . ', ' . ', ' R ', ' . ', ' . ' ],
+        ];
+
+        $board = (new Ascii())->toBoard($position, Symbol::WHITE);
+
+        $expected = [
+            Symbol::WHITE => 2,
+            Symbol::BLACK => 1,
+        ];
+
+        $isolatedPawnEvald = (new IsolatedPawnEvaluation($board))->evaluate();
+
+        $this->assertEquals($expected, $isolatedPawnEvald);
+    }
+}
