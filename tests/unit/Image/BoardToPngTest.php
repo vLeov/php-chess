c5b74558c8bb7b5fa9e59ed27410e9ed5cc10116
Rafael Neris <72398144+rafael-neris@users.noreply.github.com>
2021-10-10 06:06:24 -0300

    Adding Board to PNG of Queens Gambit Symmetrical Defense (#89)

--- tests/unit/Image/BoardToPngTest.php
+++ tests/unit/Image/BoardToPngTest.php
@@ -9,6 +9,7 @@ use Chess\Tests\AbstractUnitTestCase;
 use Chess\Tests\Sample\Opening\Benoni\BenkoGambit;
 use Chess\Tests\Sample\Opening\Benoni\FianchettoVariation as BenoniFianchettoVariation;
 use Chess\Tests\Sample\Opening\Sicilian\Open as OpenSicilian;
+use Chess\Tests\Sample\Opening\QueensGambit\SymmetricalDefense as QueensGambitSymmetricalDefense;
 
 class BoardToPngTest extends AbstractUnitTestCase
 {
@@ -93,6 +94,21 @@ class BoardToPngTest extends AbstractUnitTestCase
         $this->assertEquals(
             md5(file_get_contents(self::OUTPUT_FOLDER . '/tmp.png')),
             md5(file_get_contents(self::DATA_FOLDER . '/img/open_sicilian.png'))
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function output_symmetrical_defense_to_the_queens_gambit()
+    {
+        $board = (new QueensGambitSymmetricalDefense())->play();
+
+        (new BoardToPng($board))->output(self::OUTPUT_FOLDER . '/tmp.png');
+
+        $this->assertEquals(
+            md5(file_get_contents(self::OUTPUT_FOLDER . '/tmp.png')),
+            md5(file_get_contents(self::DATA_FOLDER . '/img/symmetrical_defense_to_the_queens_gambit.png'))
         );
     }
 
