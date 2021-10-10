4e77a67d6129ddc5f4e606a8c400cee1a04e9c9b
Rafael Neris <72398144+rafael-neris@users.noreply.github.com>
2021-10-10 17:37:40 -0300

    Adding image test of Ruy Lopez, Lucena Defense (#90)
    * Adding Board to PNG of Queens Gambit Symmetrical Defense
    
    * Adding image test to Lucena Defense

--- tests/unit/Image/BoardToPngTest.php
+++ tests/unit/Image/BoardToPngTest.php
@@ -8,6 +8,8 @@ use Chess\Image\BoardToPng;
 use Chess\Tests\AbstractUnitTestCase;
 use Chess\Tests\Sample\Opening\Benoni\BenkoGambit;
 use Chess\Tests\Sample\Opening\Benoni\FianchettoVariation as BenoniFianchettoVariation;
+use Chess\Tests\Sample\Opening\RuyLopez\LucenaDefense;
+use Chess\Tests\Sample\Opening\Sicilian\Closed as ClosedSicilian;
 use Chess\Tests\Sample\Opening\Sicilian\Open as OpenSicilian;
 use Chess\Tests\Sample\Opening\QueensGambit\SymmetricalDefense as QueensGambitSymmetricalDefense;
 
@@ -94,6 +96,21 @@ class BoardToPngTest extends AbstractUnitTestCase
         $this->assertEquals(
             md5(file_get_contents(self::OUTPUT_FOLDER . '/tmp.png')),
             md5(file_get_contents(self::DATA_FOLDER . '/img/open_sicilian.png'))
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function output_lucena_defense()
+    {
+        $board = (new LucenaDefense())->play();
+
+        (new BoardToPng($board))->output(self::OUTPUT_FOLDER . '/tmp.png');
+
+        $this->assertEquals(
+            md5(file_get_contents(self::OUTPUT_FOLDER . '/tmp.png')),
+            md5(file_get_contents(self::DATA_FOLDER . '/img/lucena_defense.png'))
         );
     }
 
