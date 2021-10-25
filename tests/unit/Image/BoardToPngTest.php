66902abda5c91f08d600af0ec7e6af2d9faa8fd0
Jordi Bassaganas <info@programarivm.com>
2021-10-25 19:22:57 +0200

    Added test output_closed_sicilian() (#116)

--- tests/unit/Image/BoardToPngTest.php
+++ tests/unit/Image/BoardToPngTest.php
@@ -133,6 +133,21 @@ class BoardToPngTest extends AbstractUnitTestCase
     /**
      * @test
      */
+    public function output_closed_sicilian()
+    {
+        $board = (new ClosedSicilian())->play();
+
+        (new BoardToPng($board))->output(self::OUTPUT_FOLDER . '/tmp.png');
+
+        $this->assertEquals(
+            md5(file_get_contents(self::OUTPUT_FOLDER . '/tmp.png')),
+            md5(file_get_contents(self::DATA_FOLDER . '/img/closed_sicilian.png'))
+        );
+    }
+
+    /**
+     * @test
+     */
     public function output_lucena_defense()
     {
         $board = (new LucenaDefense())->play();
