36baed011a825a3723d91207fa043ff9f703db70
programarivm <info@programarivm.com>
2022-07-14 17:30:28 +0200

    Implemented Chess\ML\Supervised\AbstractPredictor

---
+++ src/ML/Supervised/AbstractPredictor.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Chess\ML\Supervised;
+
+use Chess\Board;
+use Chess\HeuristicsByFenString;
+use Chess\PGN\AN\Color;
+use Rubix\ML\Datasets\Unlabeled;
+use Rubix\ML\PersistentModel;
+
+abstract class AbstractPredictor
+{
+    protected Board $board;
+
+    protected PersistentModel $estimator;
+
+    protected array $result = [];
+
+    public function __construct(Board $board, PersistentModel $estimator)
+    {
+        $this->board = $board;
+        $this->estimator = $estimator;
+    }
+
+    protected function sort(string $color): AbstractPredictor
+    {
+        usort($this->result, function ($a, $b) use ($color) {
+            if ($color === Color::W) {
+                $current = current($b)['diff'] <=> current($a)['diff'];
+            } else {
+                $current = current($a)['diff'] <=> current($b)['diff'];
+            }
+            return $current;
+        });
+
+        return $this;
+    }
+
+    public function predict(): string
+    {
+        $balance = (new HeuristicsByFenString($this->board->toFen()))->getResizedBalance(0, 1);
+        $dataset = new Unlabeled([$balance]);
+        $prediction = current($this->estimator->predict($dataset));
+        $color = $this->board->getTurn();
+        foreach ($this->board->legalMoves() as $legalMove) {
+            $clone = unserialize(serialize($this->board));
+            $clone->play($color, $legalMove);
+            $this->result[] = [
+                $legalMove => $this->eval($clone, $prediction)
+            ];
+        }
+        $this->sort($color);
+        $current = current(array_keys($this->result[0]));
+
+        return $current;
+    }
+}
--- src/ML/Supervised/Classification/PermutationPredictor.php
+++ src/ML/Supervised/Classification/PermutationPredictor.php
@@ -5,31 +5,23 @@ namespace Chess\ML\Supervised\Classification;
 use Chess\Board;
 use Chess\HeuristicsByFenString;
 use Chess\Combinatorics\RestrictedPermutationWithRepetition;
-use Chess\PGN\AN\Color;
-use Rubix\ML\PersistentModel;
-use Rubix\ML\Datasets\Unlabeled;
+use Chess\ML\Supervised\AbstractPredictor;
 
-class PermutationPredictor
+class PermutationPredictor extends AbstractPredictor
 {
-    protected Board $board;
-
-    protected PersistentModel $estimator;
-
-    protected array $result = [];
-
     public function __construct(Board $board, PersistentModel $estimator)
     {
-        $this->board = $board;
-        $this->estimator = $estimator;
+        parent::__construct($board, $estimator);
+
         $this->permutations = (new RestrictedPermutationWithRepetition())
             ->get(
                 [4, 24],
-                count((new HeuristicsByFenString(''))->getDimensions()),
+                count((new Heuristics(''))->getDimensions()),
                 100
             );
     }
 
-    protected function eval(Board $clone, $prediction): array
+    protected function eval(Board $clone, int $prediction): array
     {
         $balance = (new HeuristicsByFenString($clone->toFen()))->getResizedBalance(0, 1);
         $label = (new PermutationLabeller($this->permutations))
@@ -37,41 +29,7 @@ class PermutationPredictor
 
         return [
             'label' => $label,
-            'prediction' => $prediction,
             'diff' => abs($label - $prediction)
         ];
-    }
-
-    protected function sort(string $color): PermutationPredictor
-    {
-        usort($this->result, function ($a, $b) use ($color) {
-            if ($color === Color::W) {
-                $current = current($b)['diff'] <=> current($a)['diff'];
-            } else {
-                $current = current($a)['diff'] <=> current($b)['diff'];
-            }
-            return $current;
-        });
-
-        return $this;
-    }
-
-    public function predict(): string
-    {
-        $balance = (new HeuristicsByFenString($this->board->toFen()))->getResizedBalance(0, 1);
-        $dataset = new Unlabeled([$balance]);
-        $prediction = current($this->estimator->predict($dataset));
-        $color = $this->board->getTurn();
-        foreach ($this->board->legalMoves() as $legalMove) {
-            $clone = unserialize(serialize($this->board));
-            $clone->play($color, $legalMove);
-            $this->result[] = [
-                $legalMove => $this->eval($clone, $prediction)
-            ];
-        }
-        $this->sort($color);
-        $current = current(array_keys($this->result[0]));
-
-        return $current;
     }
 }
