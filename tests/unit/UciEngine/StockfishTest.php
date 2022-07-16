b01a525379448caf29086395d5fb5472be2d918e
programarivm <info@programarivm.com>
2022-07-17 00:39:53 +0200

    Updated the tests

--- tests/unit/UciEngine/StockfishTest.php
+++ tests/unit/UciEngine/StockfishTest.php
@@ -3,7 +3,7 @@
 namespace Chess\Tests\Unit\UciEngine;
 
 use Chess\Board;
-use Chess\FEN\StrToBoard;
+use Chess\FEN\ShortStrToPgn;
 use Chess\UciEngine\Stockfish;
 use Chess\Tests\AbstractUnitTestCase;
 
@@ -12,20 +12,38 @@ class StockfishTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function fen()
+    public function shortFen()
     {
         $board = new Board();
         $board->play('w', 'e4');
-
         $stockfish = new Stockfish($board);
-        $fen = $stockfish->fen($board->toFen(), 3);
 
-        $board = (new StrToBoard($fen))->create();
-        $board->play('w', 'Ke2');
+        $fromFen = $board->toFen();
+        $toFen = $stockfish->shortFen($fromFen, 3);
 
+        $this->assertNotEmpty($toFen);
+    }
+
+    /**
+     * @test
+     */
+    public function play()
+    {
+        $board = new Board();
+        $board->play('w', 'e4');
         $stockfish = new Stockfish($board);
-        $fen = $stockfish->fen($board->toFen(), 3);
 
-        $this->assertNotEmpty($fen);
+        $fromFen = $board->toFen();
+        $toFen = $stockfish->shortFen($fromFen, 3);
+        $pgn = (new ShortStrToPgn($fromFen, $toFen))->create();
+
+        $this->assertTrue($board->play('b', current($pgn)));
+        $this->assertTrue($board->play('w', 'a3'));
+
+        $fromFen = $board->toFen();
+        $toFen = $stockfish->shortFen($fromFen, 3);
+        $pgn = (new ShortStrToPgn($fromFen, $toFen))->create();
+
+        $this->assertTrue($board->play('b', current($pgn)));
     }
 }
