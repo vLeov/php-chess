7993232711924722b5f9ebf3308bb18d7c2b37b2
Jordi Bassaganas <info@programarivm.com>
2022-07-22 23:11:32 +0200

    Feature/randomizer (#218)
    * TDDed the kings() method
    
    * TDDed the pieces() method
    
    * Refactored Chess\Randomizer
    
    * TDDed the pieces() method
    
    * Refactored Chess\Randomizer
    
    * TDDed Chess\Randomizer
    
    * Added test

---
+++ tests/unit/RandomizerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Chess\Tests\Unit;
+
+use Chess\Randomizer;
+use Chess\FEN\BoardToStr;
+use Chess\PGN\AN\Color;
+use Chess\Tests\AbstractUnitTestCase;
+
+class RandomizerTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function kings()
+    {
+        $turn = Color::W;
+
+        $board = (new Randomizer($turn))->getBoard();
+
+        $fen = (new BoardToStr($board))->create();
+
+        $this->assertNotEmpty($fen);
+    }
+
+    /**
+     * @test
+     */
+    public function w_N_B_R()
+    {
+        $turn = Color::W;
+
+        $items = [
+            Color::W => ['N', 'B', 'R'],
+        ];
+
+        $board = (new Randomizer($turn, $items))->getBoard();
+
+        $fen = (new BoardToStr($board))->create();
+
+        $this->assertNotEmpty($fen);
+    }
+
+    /**
+     * @test
+     */
+    public function b_N_B_R()
+    {
+        $turn = Color::B;
+
+        $items = [
+            Color::B => ['N', 'B', 'R'],
+        ];
+
+        $board = (new Randomizer($turn, $items))->getBoard();
+
+        $fen = (new BoardToStr($board))->create();
+
+        $this->assertNotEmpty($fen);
+    }
+}
