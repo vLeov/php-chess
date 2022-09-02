3895723a76effb905ea6882a1c102863bf47b285
programarivm <info@programarivm.com>
2022-09-02 13:07:53 +0200

    Added tags

--- src/PGN/Tag.php
+++ src/PGN/Tag.php
@@ -12,65 +12,69 @@ use Chess\Exception\UnknownNotationException;
  */
 class Tag extends AbstractNotation
 {
-	// STR (Seven Tag Roster)
+    // STR (Seven Tag Roster)
     const EVENT = 'Event';
-	const SITE = 'Site';
-	const DATE = 'Date';
-	const ROUND = 'Round';
-	const WHITE = 'White';
-	const BLACK = 'Black';
-	const RESULT = 'Result';
+    const SITE = 'Site';
+    const DATE = 'Date';
+    const ROUND = 'Round';
+    const WHITE = 'White';
+    const BLACK = 'Black';
+    const RESULT = 'Result';
 
     // FICS database
     const FICS_GAMES_DB_GAME_NO = 'FICSGamesDBGameNo';
 
-	// player related information
-	const WHITE_TITLE = 'WhiteTitle';
-	const BLACK_TITLE = 'BlackTitle';
-	const WHITE_ELO = 'WhiteElo';
-	const BLACK_ELO = 'BlackElo';
-	const WHITE_USCF = 'WhiteUSCF';
-	const BLACK_USCF = 'BlackUSCF';
-	const WHITE_NA = 'WhiteNA';
-	const BLACK_NA = 'BlackNA';
-	const WHITE_TYPE = 'WhiteType';
-	const BLACK_TYPE = 'BlackType';
+    // player related information
+    const WHITE_TITLE = 'WhiteTitle';
+    const BLACK_TITLE = 'BlackTitle';
+    const WHITE_ELO = 'WhiteElo';
+    const BLACK_ELO = 'BlackElo';
+    const WHITE_USCF = 'WhiteUSCF';
+    const BLACK_USCF = 'BlackUSCF';
+    const WHITE_NA = 'WhiteNA';
+    const BLACK_NA = 'BlackNA';
+    const WHITE_TYPE = 'WhiteType';
+    const BLACK_TYPE = 'BlackType';
+    const WHITE_FIDE_ID = 'WhiteFideId';
+    const BLACK_FIDE_ID = 'BlackFideId';
+    const WHITE_TEAM = 'WhiteTeam';
+    const BLACK_TEAM = 'BlackTeam';
 
-	// event related information
-	const EVENT_DATE = 'EventDate';
-	const EVENT_SPONSOR = 'EventSponsor';
-	const SECTION = 'Section';
-	const STAGE = 'Stage';
-	const BOARD = 'Board';
+    // event related information
+    const EVENT_DATE = 'EventDate';
+    const EVENT_SPONSOR = 'EventSponsor';
+    const SECTION = 'Section';
+    const STAGE = 'Stage';
+    const BOARD = 'Board';
 
-	// opening information
-	const OPENING = 'Opening';
-	const VARIATION = 'Variation';
-	const SUB_VARIATION = 'SubVariation';
-	const ECO = 'ECO';
-	const NIC = 'NIC';
+    // opening information
+    const OPENING = 'Opening';
+    const VARIATION = 'Variation';
+    const SUB_VARIATION = 'SubVariation';
+    const ECO = 'ECO';
+    const NIC = 'NIC';
 
-	// time and date related information
-	const TIME = 'Time';
+    // time and date related information
+    const TIME = 'Time';
     const TIME_CONTROL = 'TimeControl';
-	const UTC_TIME = 'UTCTime';
-	const UTC_DATE = 'UTCDate';
+    const UTC_TIME = 'UTCTime';
+    const UTC_DATE = 'UTCDate';
 
     // clock
     const WHITE_CLOCK = 'WhiteClock';
     const BLACK_CLOCK = 'BlackClock';
 
-	// alternative starting positions
-	const SET_UP = 'SetUp';
-	const FEN = 'FEN';
+    // alternative starting positions
+    const SET_UP = 'SetUp';
+    const FEN = 'FEN';
 
-	// game conclusion
-	const TERMINATION = 'Termination';
+    // game conclusion
+    const TERMINATION = 'Termination';
 
-	// miscellaneous
-	const ANNOTATOR = 'Annotator';
-	const MODE = 'Mode';
-	const PLY_COUNT = 'PlyCount';
+    // miscellaneous
+    const ANNOTATOR = 'Annotator';
+    const MODE = 'Mode';
+    const PLY_COUNT = 'PlyCount';
     const WHITE_RD = 'WhiteRD';
     const BLACK_RD = 'BlackRD';
 
@@ -84,7 +88,7 @@ class Tag extends AbstractNotation
     public static function validate(string $tag): object
     {
         $isValid = false;
-        
+
         foreach (self::values() as $val) {
             if (preg_match('/^\[' . $val . ' \"(.*)\"\]$/', $tag)) {
                 $isValid = true;
