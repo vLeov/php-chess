475ce42b34635a311ef740f8c26aee119d44a4d4
programarivm <info@programarivm.com>
2022-09-18 16:04:50 +0200

    Added test

--- tests/unit/Variant/Chess960/BoardTest.php
+++ tests/unit/Variant/Chess960/BoardTest.php
@@ -101,4 +101,83 @@ class BoardTest extends AbstractUnitTestCase
 
         $this->assertEquals($expected, $castlingRule);
     }
+
+    /**
+     * @test
+     */
+    public function get_castling_rule_Q_R_B_K_R_B_N_N()
+    {
+        $startPosition = ['Q', 'R', 'B', 'K', 'R', 'B', 'N', 'N'];
+
+        $castlingRule = (new Board($startPosition))->getCastlingRule();
+
+        $expected = [
+            Color::W => [
+                Piece::K => [
+                    Castle::SHORT => [
+                        'sqs' => [ 'f1', 'g1' ],
+                        'sq' => [
+                            'current' => 'd1',
+                            'next' => 'g1',
+                        ],
+                    ],
+                    Castle::LONG => [
+                        'sqs' => [ 'c1' ],
+                        'sq' => [
+                            'current' => 'd1',
+                            'next' => 'c1',
+                        ],
+                    ],
+                ],
+                Piece::R => [
+                    Castle::SHORT => [
+                        'sq' => [
+                            'current' => 'e1',
+                            'next' => 'f1',
+                        ],
+                    ],
+                    Castle::LONG => [
+                        'sq' => [
+                            'current' => 'b1',
+                            'next' => 'd1',
+                        ],
+                    ],
+                ],
+            ],
+            Color::B => [
+                Piece::K => [
+                    Castle::SHORT => [
+                        'sqs' => [ 'f8', 'g8' ],
+                        'sq' => [
+                            'current' => 'd8',
+                            'next' => 'g8',
+                        ],
+                    ],
+                    Castle::LONG => [
+                        'sqs' => [ 'c8' ],
+                        'sq' => [
+                            'current' => 'd8',
+                            'next' => 'c8',
+                        ],
+                    ],
+                ],
+                Piece::R => [
+                    Castle::SHORT => [
+                        'sq' => [
+                            'current' => 'e8',
+                            'next' => 'f8',
+                        ],
+                    ],
+                    Castle::LONG => [
+                        'sq' => [
+                            'current' => 'b8',
+                            'next' => 'd8',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $this->assertEquals($expected, $castlingRule);
+    }
 }
