9210552465743ae4bc3fce21ad3cf69cbff84301
programarivm <info@programarivm.com>
2022-10-06 15:58:26 +0200

    Fixed Chess\Variant\Classical\FEN\Str

--- src/Variant/Classical/FEN/Str.php
+++ src/Variant/Classical/FEN/Str.php
@@ -44,21 +44,21 @@ class Str
     {
         $array = [];
         $ranks = array_reverse(explode('/', $string));
-        foreach ($ranks as $rank) {
+        for ($i = count($ranks) - 1; $i >= 0; $i--) {
             $row = [];
-            preg_match_all('!\d+!', $rank, $digits, PREG_OFFSET_CAPTURE);
-            preg_match_all('/[a-zA-Z]{1}/', $rank, $letters, PREG_OFFSET_CAPTURE);
+            preg_match_all('!\d+!', $ranks[$i], $digits, PREG_OFFSET_CAPTURE);
+            preg_match_all('/[a-zA-Z]{1}/', $ranks[$i], $letters, PREG_OFFSET_CAPTURE);
             $all = array_merge($digits[0], $letters[0]);
             usort($all, function ($a, $b) {
                 return $a[1] <=> $b[1];
             });
-            foreach ($all as $key => $val) {
-                !is_numeric($val[0])
-                    ? $elem = [" {$val[0]} "]
-                    : $elem = array_fill(0, $val[0], ' . ');
+            foreach ($all as $item) {
+                !is_numeric($item[0])
+                    ? $elem = [" {$item[0]} "]
+                    : $elem = array_fill(0, $item[0], ' . ');
                 $row = array_values(array_merge($row, $elem));
             }
-            $array[] = $row;
+            $array[$i] = $row;
         }
 
         return $array;
