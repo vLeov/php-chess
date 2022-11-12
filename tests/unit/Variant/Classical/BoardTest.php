d6c8738a4ae8e8068884a213af3b1183bc3add89
programarivm <info@programarivm.com>
2022-11-12 12:25:16 +0100

    Added tests

--- tests/unit/Variant/Classical/BoardTest.php
+++ tests/unit/Variant/Classical/BoardTest.php
@@ -2514,4 +2514,62 @@ class BoardTest extends AbstractUnitTestCase
         $this->assertTrue($board->playLan('w', 'b1a3'));
         $this->assertTrue($board->playLan('b', 'd7d5'));
     }
+
+    /**
+     * @test
+     */
+    public function play_lan_d2d3_d7d6___b8d7_d2f3()
+    {
+        $board = new Board();
+
+        $this->assertTrue($board->playLan('w', 'd2d3'));
+        $this->assertTrue($board->playLan('b', 'd7d6'));
+        $this->assertTrue($board->playLan('w', 'e2e3'));
+        $this->assertTrue($board->playLan('b', 'e7e6'));
+        $this->assertTrue($board->playLan('w', 'b1d2'));
+        $this->assertTrue($board->playLan('b', 'b8d7'));
+        $this->assertTrue($board->playLan('w', 'd2f3'));
+
+        $expected = [
+            7 => [ ' r ', ' . ', ' b ', ' q ', ' k ', ' b ', ' n ', ' r ' ],
+            6 => [ ' p ', ' p ', ' p ', ' n ', ' . ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' p ', ' p ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' P ', ' P ', ' N ', ' . ', ' . ' ],
+            1 => [ ' P ', ' P ', ' P ', ' . ', ' . ', ' P ', ' P ', ' P ' ],
+            0 => [ ' R ', ' . ', ' B ', ' Q ', ' K ', ' B ', ' N ', ' R ' ],
+        ];
+
+        $this->assertSame($expected, $board->toAsciiArray());
+    }
+
+    /**
+     * @test
+     */
+    public function play_d3_d6___Nd7_Nf3()
+    {
+        $board = new Board();
+
+        $this->assertTrue($board->play('w', 'd3'));
+        $this->assertTrue($board->play('b', 'd6'));
+        $this->assertTrue($board->play('w', 'e3'));
+        $this->assertTrue($board->play('b', 'e6'));
+        $this->assertTrue($board->play('w', 'Nd2'));
+        $this->assertTrue($board->play('b', 'Nd7'));
+        $this->assertTrue($board->play('w', 'Nf3'));
+
+        $expected = [
+            7 => [ ' r ', ' . ', ' b ', ' q ', ' k ', ' b ', ' n ', ' r ' ],
+            6 => [ ' p ', ' p ', ' p ', ' n ', ' . ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' p ', ' p ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' P ', ' P ', ' N ', ' . ', ' . ' ],
+            1 => [ ' P ', ' P ', ' P ', ' . ', ' . ', ' P ', ' P ', ' P ' ],
+            0 => [ ' R ', ' . ', ' B ', ' Q ', ' K ', ' B ', ' N ', ' R ' ],
+        ];
+
+        $this->assertSame($expected, $board->toAsciiArray());
+    }
 }
