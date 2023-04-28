684d02050c0bf98a2ef9f1e00e29aea6ec53c213
programarivm <info@programarivm.com>
2023-04-28 14:04:02 +0200

    Added test

--- tests/unit/Variant/Chess960/FEN/StrToBoardTest.php
+++ tests/unit/Variant/Chess960/FEN/StrToBoardTest.php
@@ -182,4 +182,36 @@ class StrToBoardTest extends AbstractUnitTestCase
 
         $this->assertSame($expected, $array);
     }
+
+    /**
+     * @test
+     */
+    public function QNRNBKRB_O_O_undo()
+    {
+        $startPos = ['Q', 'N', 'R', 'N', 'B', 'K', 'R', 'B' ];
+
+        $board = (new StrToBoard(
+            'qnrnbkrb/pppppppp/8/8/8/8/PPPPPPPP/QNRNBKRB w KQkq -',
+            $startPos
+        ))->create();
+
+        $board->play('w', 'O-O');
+
+        $board = $board->undo();
+
+        $array = $board->toAsciiArray();
+
+        $expected = [
+            7 => [ ' q ', ' n ', ' r ', ' n ', ' b ', ' k ', ' r ', ' b ' ],
+            6 => [ ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            1 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
+            0 => [ ' Q ', ' N ', ' R ', ' N ', ' B ', ' K ', ' R ', ' B ' ],
+        ];
+
+        $this->assertSame($expected, $array);
+    }
 }
