72e494175ca325270daf812447038e6bac30a408
programarivm <info@programarivm.com>
2023-06-22 13:17:12 +0200

    Added tests

--- tests/unit/Play/RavTest.php
+++ tests/unit/Play/RavTest.php
@@ -38,4 +38,65 @@ class RavTest extends AbstractUnitTestCase
 
         $this->assertSame($expected, (new RAV($movetext))->getBreakdown());
     }
+
+    /**
+     * @test
+     */
+    public function breakdown_ellipsis_Nc6__h5()
+    {
+        $movetext = '2...Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+
+        $breakdown = (new RAV($movetext))->getBreakdown();
+
+        $expected = [
+            '2...Nc6 3.Bb5 Nf6 4.Nc3 Be7 5.d3 d6 6.Be3 Bd7 7.Qd2 a6 8.Ba4 b5 9.Bb3 O-O 10.O-O-O b4 11.Nd5',
+            '11.Nb1 h6 12.h4',
+            '12.Nh4 g5 13.Nf5',
+            '12...a5 13.g4 Nxg4',
+            '11...Nxd5 12.Bxd5 Rb8 13.h4 h6 14.Rdg1 a5 15.g4 g5 16.h5',
+            '16.hxg5 Bxg5 17.Nxg5 hxg5 18.Rh5',
+        ];
+
+        $this->assertSame($expected, (new RAV($movetext))->getBreakdown());
+    }
+
+    /**
+     * @test
+     */
+    public function breakdown_Ra7_Kg8__Rc8()
+    {
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8) 6...Kb8 (6...Kd8 7.Ra8#) 7.Rc7 Ka8 8.Kc6 Kb8 9.Kb6 Ka8 10.Rc8#';
+
+        $expected = [
+            '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8',
+            '5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#',
+            '6.Kd6',
+            '6.Kc6 Kd8',
+            '6...Kb8',
+            '6...Kd8 7.Ra8#',
+            '7.Rc7 Ka8 8.Kc6 Kb8 9.Kb6 Ka8 10.Rc8#',
+        ];
+
+        $this->assertSame($expected, (new RAV($movetext))->getBreakdown());
+    }
+
+    /**
+     * @test
+     */
+    public function breakdown_ellipsis_Kg8__Rc8()
+    {
+        $movetext = '1...Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8) 6...Kb8 (6...Kd8 7.Ra8#) 7.Rc7 Ka8 8.Kc6 Kb8 9.Kb6 Ka8 10.Rc8#';
+
+        $expected = [
+            '1...Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8',
+            '5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#',
+            '6.Kd6',
+            '6.Kc6 Kd8',
+            '6...Kb8',
+            '6...Kd8 7.Ra8#',
+            '7.Rc7 Ka8 8.Kc6 Kb8 9.Kb6 Ka8 10.Rc8#',
+        ];
+
+        $this->assertSame($expected, (new RAV($movetext))->getBreakdown());
+    }
 }
