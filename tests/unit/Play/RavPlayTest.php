a94fc32974e9575a65caf6eb39aa3bb49af55456
programarivm <info@programarivm.com>
2023-06-25 14:55:15 +0200

    Added tests

--- tests/unit/Play/RavPlayTest.php
+++ tests/unit/Play/RavPlayTest.php
@@ -190,4 +190,16 @@ class RavPlayTest extends AbstractUnitTestCase
 
         $this->assertSame($expected, (new RavPlay($movetext))->getFen());
     }
+
+    /**
+     * @test
+     */
+    public function inline_e4_e5__h5()
+    {
+        $movetext = '1. e4 e5 {foo} 2. Nf3 {bar} Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 {foobar} (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+
+        $expected = '1.e4 e5 {foo} 2.Nf3 {bar} Nc6 3.Bb5 Nf6 4.Nc3 Be7 5.d3 d6 6.Be3 Bd7 7.Qd2 a6 8.Ba4 b5 9.Bb3 O-O 10.O-O-O b4 11.Nd5 {foobar} (11.Nb1 h6 12.h4 (12.Nh4 g5 13.Nf5) 12...a5 13.g4 Nxg4) 11...Nxd5 12.Bxd5 Rb8 13.h4 h6 14.Rdg1 a5 15.g4 g5 16.h5 (16.hxg5 Bxg5 17.Nxg5 hxg5 18.Rh5)';
+
+        $this->assertSame($expected, (new RavPlay($movetext))->getRavMovetext()->inline());
+    }
 }
