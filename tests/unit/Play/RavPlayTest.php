1e05807b1e597f6cc90f0371c6e396fa2b2940e4
programarivm <info@programarivm.com>
2023-07-01 13:41:12 +0200

    Updated tests

--- tests/unit/Play/RavPlayTest.php
+++ tests/unit/Play/RavPlayTest.php
@@ -24,7 +24,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function breakdown_e4_e5__h5()
     {
-        $movetext = '1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+        $movetext = '1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5
+            (11. Nb1 h6 12. h4
+            (12. Nh4 g5 13. Nf5)
+            12... a5 13. g4 Nxg4)
+            11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5
+            (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
 
         $breakdown = (new RavPlay($movetext))->getBreakdown();
 
@@ -45,7 +50,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function breakdown_starting_with_ellipsis_Nc6__h5()
     {
-        $movetext = '2...Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+        $movetext = '2...Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5
+            (11. Nb1 h6 12. h4
+            (12. Nh4 g5 13. Nf5)
+            12... a5 13. g4 Nxg4)
+            11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5
+            (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
 
         $breakdown = (new RavPlay($movetext))->getBreakdown();
 
@@ -66,7 +76,13 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function breakdown_Ra7_Kg8__Rc8()
     {
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8) 6...Kb8 (6...Kd8 7.Ra8#) 7.Rc7 Ka8 8.Kc6 Kb8 9.Kb6 Ka8 10.Rc8#';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)
+            6.Kd6
+            (6.Kc6 Kd8)
+            6...Kb8
+            (6...Kd8 7.Ra8#)
+            7.Rc7 Ka8 8.Kc6 Kb8 9.Kb6 Ka8 10.Rc8#';
 
         $expected = [
             '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8',
@@ -86,7 +102,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function breakdown_Ke2_Kd5__Ra1()
     {
-        $movetext = '1.Ke2 Kd5 2.Ke3 Kc4 (2...Ke5 3.Rh5+) (2...Kc4 3.Rh5) 3.Rh5 (3...Kb4 4.Kd3) 3...Kc3 4.Rh4 Kc2 5.Rc4+ Kb3 6.Kd3 Kb2 7.Rb4+ Ka3 8.Kc3 Ka2 9.Ra4+ Kb1 10.Ra5 Kc1 11.Ra1#';
+        $movetext = '1.Ke2 Kd5 2.Ke3 Kc4
+            (2...Ke5 3.Rh5+)
+            (2...Kc4 3.Rh5)
+            3.Rh5
+            (3...Kb4 4.Kd3)
+            3...Kc3 4.Rh4 Kc2 5.Rc4+ Kb3 6.Kd3 Kb2 7.Rb4+ Ka3 8.Kc3 Ka2 9.Ra4+ Kb1 10.Ra5 Kc1 11.Ra1#';
 
         $expected = [
             '1.Ke2 Kd5 2.Ke3 Kc4',
@@ -105,7 +126,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function breakdown_starting_with_ellipsis_Kc4__Ra1()
     {
-        $movetext = '2...Kc4 (2...Ke5 3.Rh5+) (2...Kc4 3.Rh5) 3.Rh5 (3...Kb4 4.Kd3) 3...Kc3 4.Rh4 Kc2 5.Rc4+ Kb3 6.Kd3 Kb2 7.Rb4+ Ka3 8.Kc3 Ka2 9.Ra4+ Kb1 10.Ra5 Kc1 11.Ra1#';
+        $movetext = '2...Kc4
+            (2...Ke5 3.Rh5+)
+            (2...Kc4 3.Rh5)
+            3.Rh5
+            (3...Kb4 4.Kd3)
+            3...Kc3 4.Rh4 Kc2 5.Rc4+ Kb3 6.Kd3 Kb2 7.Rb4+ Ka3 8.Kc3 Ka2 9.Ra4+ Kb1 10.Ra5 Kc1 11.Ra1#';
 
         $expected = [
             '2...Kc4',
@@ -124,7 +150,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function play_e4_e5__h5()
     {
-        $movetext = '1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+        $movetext = '1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5
+            (11. Nb1 h6 12. h4
+            (12. Nh4 g5 13. Nf5)
+            12... a5 13. g4 Nxg4)
+            11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5
+            (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
 
         $board = (new RavPlay($movetext))->validate()->getBoard();
 
@@ -138,7 +169,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function fen_e4_e5__h5()
     {
-        $movetext = '1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+        $movetext = '1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5
+            (11. Nb1 h6 12. h4
+            (12. Nh4 g5 13. Nf5)
+            12... a5 13. g4 Nxg4)
+            11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5
+            (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
 
         $expected = [
             'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -',
@@ -197,7 +233,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function filtered_e4_e5__h5()
     {
-        $movetext = '1. e4 e5 {foo} 2. Nf3 {bar} Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 {foobar} (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+        $movetext = '1. e4 e5 {foo} 2. Nf3 {bar} Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 {foobar}
+            (11. Nb1 h6 12. h4
+            (12. Nh4 g5 13. Nf5)
+            12... a5 13. g4 Nxg4)
+            11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5
+            (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
 
         $expected = '1.e4 e5 {foo} 2.Nf3 {bar} Nc6 3.Bb5 Nf6 4.Nc3 Be7 5.d3 d6 6.Be3 Bd7 7.Qd2 a6 8.Ba4 b5 9.Bb3 O-O 10.O-O-O b4 11.Nd5 {foobar} (11.Nb1 h6 12.h4 (12.Nh4 g5 13.Nf5) 12...a5 13.g4 Nxg4) 11...Nxd5 12.Bxd5 Rb8 13.h4 h6 14.Rdg1 a5 15.g4 g5 16.h5 (16.hxg5 Bxg5 17.Nxg5 hxg5 18.Rh5)';
 
@@ -288,7 +329,12 @@ class RavPlayTest extends AbstractUnitTestCase
      */
     public function play_d4_d5__Nxb5_Ng4()
     {
-        $movetext = '1. e4 e5 {foo} 2. Nf3 {bar} Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 {foobar} (11. Nb1 h6 12. h4 (12. Nh4 g5 13. Nf5) 12... a5 13. g4 Nxg4) 11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5 (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
+        $movetext = '1. e4 e5 {foo} 2. Nf3 {bar} Nc6 3. Bb5 Nf6 4. Nc3 Be7 5. d3 d6 6. Be3 Bd7 7. Qd2 a6 8. Ba4 b5 9. Bb3 O-O 10. O-O-O b4 11. Nd5 {foobar}
+            (11. Nb1 h6 12. h4
+            (12. Nh4 g5 13. Nf5)
+            12... a5 13. g4 Nxg4)
+            11... Nxd5 12. Bxd5 Rb8 13. h4 h6 14. Rdg1 a5 15. g4 g5 16. h5
+            (16. hxg5 Bxg5 17. Nxg5 hxg5 18. Rh5)';
 
         $ravPlay = new RavPlay($movetext);
         $board = $ravPlay->validate()->getBoard();
@@ -305,7 +351,12 @@ class RavPlayTest extends AbstractUnitTestCase
     {
         $fen = '7k/8/8/8/8/8/8/R6K w - -';
 
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8) 6...Kb8 (6...Kd8 7.Ra8#)';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)
+            6.Kd6
+            (6.Kc6 Kd8)
+            6...Kb8
+            (6...Kd8 7.Ra8#)';
 
         $board = (new StrToBoard($fen))->create();
 
@@ -362,7 +413,8 @@ class RavPlayTest extends AbstractUnitTestCase
     {
         $fen = '7k/8/8/8/8/8/8/R6K w - -';
 
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)';
 
         $board = (new StrToBoard($fen))->create();
 
@@ -402,7 +454,9 @@ class RavPlayTest extends AbstractUnitTestCase
     {
         $fen = '7k/8/8/8/8/8/8/R6K w - -';
 
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)
+            6.Kd6';
 
         $board = (new StrToBoard($fen))->create();
 
@@ -443,7 +497,10 @@ class RavPlayTest extends AbstractUnitTestCase
     {
         $fen = '7k/8/8/8/8/8/8/R6K w - -';
 
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8)';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)
+            6.Kd6
+            (6.Kc6 Kd8)';
 
         $board = (new StrToBoard($fen))->create();
 
@@ -486,7 +543,11 @@ class RavPlayTest extends AbstractUnitTestCase
     {
         $fen = '7k/8/8/8/8/8/8/R6K w - -';
 
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8) 6...Kb8';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)
+            6.Kd6
+            (6.Kc6 Kd8)
+            6...Kb8';
 
         $board = (new StrToBoard($fen))->create();
 
@@ -530,7 +591,12 @@ class RavPlayTest extends AbstractUnitTestCase
     {
         $fen = '7k/8/8/8/8/8/8/R6K w - -';
 
-        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8 (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#) 6.Kd6 (6.Kc6 Kd8) 6...Kb8 (6...Kd8 7.Ra8#)';
+        $movetext = '1.Ra7 Kg8 2.Kg2 Kf8 3.Kf3 Ke8 4.Ke4 Kd8 5.Kd5 Kc8
+            (5...Ke8 6.Kd6 Kf8 7.Ke6 Kg8 8.Kf6 Kh8 9.Kg6 Kg8 10.Ra8#)
+            6.Kd6
+            (6.Kc6 Kd8)
+            6...Kb8
+            (6...Kd8 7.Ra8#)';
 
         $board = (new StrToBoard($fen))->create();
 
