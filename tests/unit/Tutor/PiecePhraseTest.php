e375315ece9ccc79e1d0ba024e55e5ada912892f
programarivm <info@programarivm.com>
2023-12-03 00:49:09 +0100

    Wording

--- tests/unit/Tutor/PiecePhraseTest.php
+++ tests/unit/Tutor/PiecePhraseTest.php
@@ -21,13 +21,13 @@ class PiecePhraseTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function deterministic_whites_knight()
+    public function predictable_whites_knight()
     {
         $expected = "White's knight on d4";
 
         $piece = new N('w', 'd4', self::$size);
 
-        $explanation = PiecePhrase::deterministic($piece);
+        $explanation = PiecePhrase::predictable($piece);
 
         $this->assertSame($expected, $explanation);
     }
@@ -35,13 +35,13 @@ class PiecePhraseTest extends AbstractUnitTestCase
     /**
      * @test
      */
-    public function deterministic_black_knight()
+    public function predictable_black_knight()
     {
         $expected = "Black's knight on e4";
 
         $piece = new N('b', 'e4', self::$size);
 
-        $explanation = PiecePhrase::deterministic($piece);
+        $explanation = PiecePhrase::predictable($piece);
 
         $this->assertSame($expected, $explanation);
     }
