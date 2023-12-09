6c797d53a58eeae06cb26b7b19ab39f5977997da
programarivm <info@programarivm.com>
2023-12-09 11:20:33 +0100

    Refactored Chess\Eval\TacticsEval

--- src/Eval/TacticsEval.php
+++ src/Eval/TacticsEval.php
@@ -5,7 +5,6 @@ namespace Chess\Eval;
 use Chess\Eval\DefenseEval;
 use Chess\Eval\PressureEval;
 use Chess\Variant\Classical\PGN\AN\Color;
-use Chess\Variant\Classical\PGN\AN\Piece;
 use Chess\Variant\Classical\Board;
 
 /**
@@ -35,11 +34,6 @@ class TacticsEval extends AbstractEval
     private array $pressEval;
 
     /**
-     * @var array[]
-     */
-    private array $target;
-
-    /**
      * @param \Chess\Variant\Classical\Board $board
      */
     public function __construct(Board $board)
@@ -49,40 +43,16 @@ class TacticsEval extends AbstractEval
         $this->defenseEval = (new DefenseEval($board))->getResult();
         $this->pressEval = (new PressureEval($board))->getResult();
 
-        $this->target = [
-            Color::W => [],
-            Color::B => [],
-        ];
-
-        $this->target();
-
-        foreach ($this->target as $color => $sqs) {
-            foreach ($sqs as $sq) {
-                $id = $this->board->getPieceBySq($sq)->getId();
-                if ($id !== Piece::K) {
-                    $this->result[$color] += self::$value[$id];
-                }
-            }
-        }
-    }
-
-    /**
-     * Calculates the squares that are not being defended.
-     *
-     * @return void
-     */
-    protected function target(): void
-    {
         foreach ($this->pressEval as $color => $sqs) {
             $countPress = array_count_values($sqs);
             $countDefense = array_count_values($this->defenseEval[Color::opp($color)]);
             foreach ($sqs as $sq) {
                 if (in_array($sq, $this->defenseEval[Color::opp($color)])) {
                     if ($countPress[$sq] > $countDefense[$sq]) {
-                        $this->target[$color][] = $sq;
+                        $this->result[$color] += self::$value[$this->board->getPieceBySq($sq)->getId()];
                     }
                 } else {
-                    $this->target[$color][] = $sq;
+                    $this->result[$color] += self::$value[$this->board->getPieceBySq($sq)->getId()];
                 }
             }
         }
