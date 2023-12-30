2d2975764a2a85d4b5b5b225343779a7d894933e
programarivm <info@programarivm.com>
2023-12-30 23:07:59 +0100

    Added test

--- tests/unit/ML/Supervised/Classification/CountLabellerTest.php
+++ tests/unit/ML/Supervised/Classification/CountLabellerTest.php
@@ -11,6 +11,25 @@ class CountLabellerTest extends AbstractUnitTestCase
     /**
      * @test
      */
+    public function start_labelled()
+    {
+        $fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+        $balance = (new FenHeuristics($fen))->getBalance();
+
+        $label = (new CountLabeller())->label($balance);
+
+        $expected = [
+            'w' => 0,
+            'b' => 0,
+        ];
+
+        $this->assertEquals($expected, $label);
+    }
+
+    /**
+     * @test
+     */
     public function e4_labelled()
     {
         $fen = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1';
