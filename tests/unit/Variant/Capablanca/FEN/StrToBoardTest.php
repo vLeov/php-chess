29f25d2af23d8dc308356707a4fc1dd581ebfe43
programarivm <info@programarivm.com>
2024-01-17 11:25:41 +0100

    Added test

--- tests/unit/Variant/Capablanca/FEN/StrToBoardTest.php
+++ tests/unit/Variant/Capablanca/FEN/StrToBoardTest.php
@@ -10,6 +10,30 @@ class StrToBoardTest extends AbstractUnitTestCase
     /**
      * @test
      */
+    public function f4()
+    {
+        $board = (new StrToBoard('rnabqkbcnr/pppppppppp/10/10/5P4/10/PPPPP1PPPP/RNABQKBCNR b KQkq f3'))
+            ->create();
+
+        $array = $board->toAsciiArray();
+
+        $expected = [
+            7 => [ ' r ', ' n ', ' a ', ' b ', ' q ', ' k ', ' b ', ' c ', ' n ', ' r ' ],
+            6 => [ ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ', ' p ' ],
+            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            4 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            3 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' P ', ' . ', ' . ', ' . ', ' . ' ],
+            2 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
+            1 => [ ' P ', ' P ', ' P ', ' P ', ' P ', ' . ', ' P ', ' P ', ' P ', ' P ' ],
+            0 => [ ' R ', ' N ', ' A ', ' B ', ' Q ', ' K ', ' B ', ' C ', ' N ', ' R ' ],
+        ];
+
+        $this->assertSame($expected, $array);
+    }
+
+    /**
+     * @test
+     */
     public function e4_e5()
     {
         $board = (new StrToBoard('rnabqkbcnr/pppp1ppppp/10/4p5/4P5/10/PPPP1PPPPP/RNABQKBCNR w KQkq e6'))
