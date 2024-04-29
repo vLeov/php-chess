c4c9f823b722fe90486d71a8e595ea1487277506
Jordi Bassagana <info@programarivm.com>
2024-04-29 17:52:46 +0200

    Implemented Chess\Computer\GuessComputer (#531)

---
+++ tests/unit/Computer/GuessComputerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Chess\Tests\Unit\Computer;
+
+use Chess\Computer\GuessComputer;
+use Chess\Play\SanPlay;
+use Chess\Tests\AbstractUnitTestCase;
+use Chess\Variant\Classical\Board;
+
+class GuessComputerTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function start()
+    {
+        $board = new Board();
+        $guessComputer = new GuessComputer();
+
+        $this->assertNotEmpty($guessComputer->move($board));
+    }
+
+    /**
+     * @test
+     */
+    public function scholars_mate_4_w()
+    {
+        $movetext = '1.e4 e5 2.Qh5 Nc6 3.Bc4 Nf6';
+        $board = (new SanPlay($movetext))->validate()->getBoard();
+        $guessComputer = new GuessComputer();
+
+        $this->assertNotEmpty($guessComputer->move($board));
+    }
+
+    /**
+     * @test
+     */
+    public function scholars_mate_4_b()
+    {
+        $movetext = '1.e4 e5 2.Qh5 Nc6 3.Bc4 Nf6 Qxf7#';
+        $board = (new SanPlay($movetext))->validate()->getBoard();
+        $guessComputer = new GuessComputer();
+
+        $this->assertSame(null, $guessComputer->move($board));
+    }
+}
