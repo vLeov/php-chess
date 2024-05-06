c529adac13cbc54b1ece3d0bc83cd2add904c457
Jordi Bassagana <info@programarivm.com>
2024-05-06 23:41:27 +0200

    Implemented Chess\Tutor\GoodPgnEvaluation (#539)

---
+++ src/Tutor/GoodPgnEvaluation.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Chess\Tutor;
+
+use Chess\UciEngine\UciEngine;
+use Chess\UciEngine\Details\Limit;
+use Chess\Variant\Classical\Board;
+
+/**
+ * GoodPgnEvaluation
+ *
+ * @author Jordi Bassagaña
+ * @license MIT
+ */
+class GoodPgnEvaluation extends AbstractParagraph
+{
+    /**
+     * UCI engine limit.
+     *
+     * @var \Chess\UciEngine\Details\Limit
+     */
+    private Limit $limit;
+
+    /**
+     * UCI engine.
+     *
+     * @var \Chess\UciEngine\Details\UciEngine
+     */
+    private UciEngine $uciEngine;
+
+    /**
+     * PGN move.
+     *
+     * @var string
+     */
+    private string $pgn;
+
+    /**
+     * Constructor.
+     *
+     * @param \Chess\UciEngine\Details\Limit $limit
+     * @param \Chess\UciEngine\Details\UciEngine $uciEngine
+     * @param \Chess\Variant\Classical\Board $board
+     */
+    public function __construct(Limit $limit, UciEngine $uciEngine, Board $board)
+    {
+        $this->limit = $limit;
+        $this->uciEngine = $uciEngine;
+        $this->board = $board;
+
+        $analysis = $uciEngine->analysis($this->board, $limit);
+
+        $clone = unserialize(serialize($this->board));
+        $clone->playLan($clone->getTurn(), $analysis['bestmove']);
+
+        $last = array_slice($clone->getHistory(), -1)[0];
+
+        $this->pgn = $last->move->pgn;
+
+        $this->paragraph = (new PgnEvaluation($this->pgn, $this->board))->getParagraph();
+    }
+
+    /**
+     * Returns the UCI engine limit.
+     *
+     * @return \Chess\UciEngine\Details\Limit
+     */
+    public function getLimit(): Limit
+    {
+        return $this->limit;
+    }
+
+    /**
+     * Returns the UCI engine.
+     *
+     * @return \Chess\UciEngine\Details\UciEngine
+     */
+    public function getUciEngine(): UciEngine
+    {
+        return $this->uciEngine;
+    }
+
+    /**
+     * Returns the PGN move.
+     *
+     * @return string
+     */
+    public function getPgn(): string
+    {
+        return $this->pgn;
+    }
+}
---
+++ tests/unit/Tutor/GoodPgnEvaluationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Chess\Tests\Unit\Tutor;
+
+use Chess\Play\SanPlay;
+use Chess\Tutor\GoodPgnEvaluation;
+use Chess\Tests\AbstractUnitTestCase;
+use Chess\UciEngine\UciEngine;
+use Chess\UciEngine\Details\Limit;
+
+class GoodPgnEvaluationTest extends AbstractUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function D07()
+    {
+        $expectedPgn = 'Bg4';
+
+        $expectedParagraph = [
+            "The black player is pressuring a little bit more squares than its opponent.",
+            "The black pieces are timidly approaching the other side's king.",
+            "Black has a moderate relative pin advantage.",
+            "The knight on e2 is pinned shielding a piece that is more valuable than the attacking piece.",
+            "Overall, 4 heuristic evaluation features are favoring White while 9 are favoring Black.",
+        ];
+
+        $limit = (new Limit())->setDepth(12);
+        $stockfish = new UciEngine('/usr/games/stockfish');
+        $A08 = file_get_contents(self::DATA_FOLDER.'/sample/D07.pgn');
+        $board = (new SanPlay($A08))->validate()->getBoard();
+
+        $goodPgnEvaluation = new GoodPgnEvaluation($limit, $stockfish, $board);
+
+        $this->assertSame($expectedPgn, $goodPgnEvaluation->getPgn());
+        $this->assertSame($expectedParagraph, $goodPgnEvaluation->getParagraph());
+    }
+}
