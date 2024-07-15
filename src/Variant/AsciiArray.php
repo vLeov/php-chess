35bf76431c601444a06802a0ee1aedf7ad6de425
programarivm <info@programarivm.com>
2024-07-15 19:23:06 +0200

    Refactored Chess\Variant\AsciiArray

--- src/Variant/AsciiArray.php
+++ src/Variant/AsciiArray.php
@@ -2,35 +2,13 @@
 
 namespace Chess\Variant;
 
-use Chess\Variant\Classical\Board;
-use Chess\Variant\Classical\PGN\AN\Square;
-
 class AsciiArray
 {
     protected array $array;
 
-    protected Square $square;
-
-    private array $castlingRule;
-
-    public function __construct(array $array, Square $square, array $castlingRule)
+    public function __construct(array $array)
     {
         $this->array = $array;
-        $this->square = $square;
-        $this->castlingRule = $castlingRule;
-    }
-
-    public function getArray(): array
-    {
-        return $this->array;
-    }
-
-    public function setElem(string $elem, string $sq): AsciiArray
-    {
-        $index = self::toIndex($sq);
-        $this->array[$index[0]][$index[1]] = $elem;
-
-        return $this;
     }
 
     public static function toIndex(string $sq): array
--- tests/unit/Variant/AsciiArrayTest.php
+++ tests/unit/Variant/AsciiArrayTest.php
@@ -4,57 +4,9 @@ namespace Chess\Tests\Unit\Variant;
 
 use Chess\Tests\AbstractUnitTestCase;
 use Chess\Variant\AsciiArray;
-use Chess\Variant\Classical\PGN\AN\Square;
-use Chess\Variant\Classical\Rule\CastlingRule;
 
 class AsciiArrayTest extends AbstractUnitTestCase
 {
-    static private Square $square;
-
-    static private CastlingRule $castlingRule;
-
-    public static function setUpBeforeClass(): void
-    {
-        self::$square = new Square();
-
-        self::$castlingRule = new CastlingRule();
-    }
-
-    /**
-     * @test
-     */
-    public function set_elem_e4_e5()
-    {
-        $array = [
-            7 => [ ' r ', ' n ', ' b ', ' q ', ' k ', ' b ', ' n ', ' r ' ],
-            6 => [ ' p ', ' p ', ' p ', ' p ', ' . ', ' p ', ' p ', ' p ' ],
-            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
-            4 => [ ' . ', ' . ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ' ],
-            3 => [ ' . ', ' . ', ' . ', ' . ', ' P ', ' . ', ' . ', ' . ' ],
-            2 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
-            1 => [ ' P ', ' P ', ' P ', ' P ', ' . ', ' P ', ' P ', ' P ' ],
-            0 => [ ' R ', ' N ', ' B ', ' Q ', ' K ', ' B ', ' N ', ' R ' ],
-        ];
-
-        $expected = [
-            7 => [ ' r ', ' n ', ' b ', ' q ', ' k ', ' b ', ' n ', ' r ' ],
-            6 => [ ' p ', ' p ', ' p ', ' p ', ' . ', ' p ', ' p ', ' p ' ],
-            5 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ' ],
-            4 => [ ' . ', ' . ', ' . ', ' . ', ' p ', ' . ', ' . ', ' . ' ],
-            3 => [ ' . ', ' . ', ' . ', ' . ', ' P ', ' . ', ' . ', ' . ' ],
-            2 => [ ' . ', ' . ', ' . ', ' . ', ' . ', ' N ', ' . ', ' . ' ],
-            1 => [ ' P ', ' P ', ' P ', ' P ', ' . ', ' P ', ' P ', ' P ' ],
-            0 => [ ' R ', ' N ', ' B ', ' Q ', ' K ', ' B ', ' . ', ' R ' ],
-        ];
-
-        $array = (new AsciiArray($array, self::$square, self::$castlingRule->rule))
-            ->setElem(' . ', 'g1')
-            ->setElem(' N ', 'f3')
-            ->getArray();
-
-        $this->assertSame($expected, $array);
-    }
-
     /**
      * @test
      */
