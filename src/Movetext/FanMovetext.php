383c5ceabcf8b1c922ac8d36cd099d8277811f54
programarivm <info@programarivm.com>
2024-08-20 12:06:12 +0200

    Refactored Chess\Movetext\FanMovetext

--- src/Movetext/FanMovetext.php
+++ src/Movetext/FanMovetext.php
@@ -16,23 +16,14 @@ class FanMovetext extends AbstractMovetext
         $this->sanMovetext = new SanMovetext($move, $this->toSan($movetext));
         $this->move = $this->sanMovetext->move;
         $this->movetext = $this->sanMovetext->movetext;
-        $this->moves = array_map(function($move) {
+        $this->moves = array_map(function ($move) {
             return $this->toFan($move);
         }, $this->sanMovetext->moves);
-
         $this->metadata = [
             'firstMove' => $this->toFan($this->sanMovetext->metadata['firstMove']),
             'lastMove' => $this->toFan($this->sanMovetext->metadata['lastMove']),
             'turn' => $this->sanMovetext->metadata['turn'],
         ];
-    }
-
-    protected function beforeInsert(): FanMovetext
-    {
-    }
-
-    protected function insert(): void
-    {
     }
 
     public function validate(): string
@@ -51,27 +42,25 @@ class FanMovetext extends AbstractMovetext
 
     private function toFan(string &$movetext): string
     {
-        $this->replace(Piece::R, '♖', $movetext);
-        $this->replace(Piece::N, '♘', $movetext);
-        $this->replace(Piece::B, '♗', $movetext);
-        $this->replace(Piece::Q, '♕', $movetext);
-        $this->replace(Piece::K, '♔', $movetext);
+        $this->replace(Piece::R, '♖', $movetext)
+            ->replace(Piece::N, '♘', $movetext)
+            ->replace(Piece::B, '♗', $movetext)
+            ->replace(Piece::Q, '♕', $movetext)
+            ->replace(Piece::K, '♔', $movetext);
 
         return $movetext;
     }
 
     private function toSan(string $movetext): string
     {
-        $movetext = str_replace('♖', Piece::R, $movetext);
-        $movetext = str_replace('♘', Piece::N, $movetext);
-        $movetext = str_replace('♗', Piece::B, $movetext);
-        $movetext = str_replace('♕', Piece::Q, $movetext);
-        $movetext = str_replace('♔', Piece::K, $movetext);
-
-        return $movetext;
+        return str_replace(
+            ['♖', '♘', '♗', '♕', '♔'],
+            [Piece::R, Piece:: N, Piece::B, Piece::Q, Piece:: K],
+            $movetext
+        );
     }
 
-    private function replace($letter, $unicode, &$movetext): void
+    private function replace($letter, $unicode, &$movetext): FanMovetext
     {
         if ($letter === Piece::K) {
             preg_match_all('/' . Move::KING . '/', $movetext, $a);
@@ -83,15 +72,19 @@ class FanMovetext extends AbstractMovetext
             preg_match_all('/' . Move::PIECE . '/', $movetext, $a);
             preg_match_all('/' . Move::PIECE_CAPTURES . '/', $movetext, $b);
         }
-        $matches = [...$a[0], ...$b[0]];
-        $this->move($letter, $unicode, $matches, $movetext);
-    }
-
-    private function move($letter, $unicode, $matches, &$movetext): void
-    {
-        array_map(function($match) use ($letter, $unicode, &$movetext) {
+        array_map(function ($match) use ($letter, $unicode, &$movetext) {
             $replaced = str_replace($letter, $unicode, $match);
             $movetext = str_replace($match, $replaced, $movetext);
-        }, $matches);
+        }, [...$a[0], ...$b[0]]);
+
+        return $this;
+    }
+
+    protected function beforeInsert(): FanMovetext
+    {
+    }
+
+    protected function insert(): void
+    {
     }
 }
